c0abbaee9a1d6ea8c0af8e8a2e39ceb8ebe4faf9
HyunSeong <hhs950120@gmail.com>
2023-01-30 21:17:39 +0900

    FEAT: 환경변수 패키지 추가 - nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BooksModule } from './books/books.module';
+
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
