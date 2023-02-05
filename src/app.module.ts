5ea62615bd3ba2b4a7bf430dcd8ffb378b9284eb
HyunSeong <hhs950120@gmail.com>
2023-02-06 00:53:34 +0900

    FEAT: MONGODB 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BooksModule } from './books/books.module';
-
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    MongooseModule.forRoot(process.env.MONGO_CONNECTION),
   ],
   controllers: [AppController],
   providers: [AppService],
