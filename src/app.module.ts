8ff03c31974fecaac10a4fbcd3778ec2a6818290
HyunSeong <hhs950120@gmail.com>
2023-02-06 01:02:57 +0900

    FEAT: MYSQL 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      cache: true,
     }),
     MongooseModule.forRoot(process.env.MONGO_CONNECTION),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: parseInt(process.env.MYSQL_PORT, 10),
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      entities: [],
+      synchronize: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
