550a703a8c27c1b478d3d2e46169bc652946a70b
HyunSeong <hhs950120@gmail.com>
2023-02-07 01:27:51 +0900

    FEAT: Logger 미들웨어 추가

---
+++ src/middlewares/logger.middleware.ts
@@ -0,0 +1,14 @@
+import { Injectable, NestMiddleware, Logger } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  private logger = new Logger('HTTP');
+
+  use(req: Request, res: Response, next: NextFunction) {
+    res.on('finish', () => {
+      this.logger.log(`${req.method} ${req.originalUrl} ${res.statusCode}`);
+    });
+    next();
+  }
+}
