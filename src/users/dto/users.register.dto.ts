0803417ea54af272630eea45196845cf06278661
HyunSeong <hhs950120@gmail.com>
2023-02-08 03:00:05 +0900

    FIX: User entity 상속 - PickType으로 필요한 멤버만 선택 - 재사용성 확보

--- src/users/dto/users.register.dto.ts
+++ src/users/dto/users.register.dto.ts
@@ -1,41 +1,17 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
+import { Users } from './../users.entity';
+import { PickType } from '@nestjs/swagger';
 
-export class UserRequestDto {
-  // Swagger에서 request 폼
-  @ApiProperty({
-    example: 'test10@naver.com',
-    description: '이메일',
-    required: true,
-  })
-  @IsEmail()
-  @IsNotEmpty()
-  email: string;
+export class UserRegisterRequestDto extends PickType(Users, [
+  'email',
+  'password',
+  'nickname',
+  'sex',
+] as const) {}
 
-  @ApiProperty({
-    example: 'efo234a08sef',
-    description: '비밀번호',
-    required: true,
-  })
-  @IsString()
-  @IsNotEmpty()
-  password: string;
-
-  @ApiProperty({
-    example: '네스트좋아',
-    description: '닉네임',
-    required: true,
-  })
-  @IsString()
-  @IsNotEmpty()
-  nickname: string;
-
-  @ApiProperty({
-    example: '남',
-    description: '성별',
-    required: true,
-  })
-  @IsString()
-  @IsNotEmpty()
-  sex: string;
-}
+export class UserRegisterResponseDto extends PickType(Users, [
+  'id',
+  'email',
+  'nickname',
+  'sex',
+  'updated_at',
+]) {}
