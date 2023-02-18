d3756b9dd2dd827ab7f8adb6847b48a66e9a6309
HyunSeong <hhs950120@gmail.com>
2023-02-19 02:48:09 +0900

    FIX: express session 미들웨어 삭제

--- src/main.ts
+++ src/main.ts
@@ -4,30 +4,11 @@ import { DocumentBuilder } from '@nestjs/swagger';
 import { SwaggerModule } from '@nestjs/swagger/dist';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './filters/http-exception.filter';
-import * as session from 'express-session';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
-  app.use(
-    /*
-      resave: 세션 수정 없어도 세션 다시 생성할 지
-      saveUninitialized: 세션에 저장할 내용 없어도 처음부터 세션 생성할 지
-      httpOnly: 클라이언트에서 쿠키 확인 가능 여부
-      secure: true면 https에서만 사용 가능
-    */
-    session({
-      secret: process.env.SESSION_SECRET,
-      resave: false,
-      saveUninitialized: false,
-      cookie: {
-        httpOnly: true,
-        secure: false,
-      },
-    }),
-  );
 
   const config = new DocumentBuilder()
     .setTitle('VOG API DOCS')
