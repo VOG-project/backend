8db2f16fadd10ad4c3787f60a979752fc291c2eb
HyunSeong <hhs950120@gmail.com>
2023-02-23 02:34:28 +0900

    FEAT: 게시물 등록 API 생성
    Controller, Service, Repository

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  UseInterceptors,
+  UseFilters,
+  Post,
+  Body,
+  UseGuards,
+} from '@nestjs/common';
+import { HttpExceptionFilter } from 'src/filters/http-exception.filter';
+import { SuccessInterceptor } from './../interceptors/success.interceptor';
+import { PostRegisterRequestDto } from './dto/post.request.dto';
+import { PostRegisterResponseDto } from './dto/post.response.dto';
+import { AuthGuard } from './../auth/guards/auth.guard';
+import { PostsService } from './posts.service';
 
 @Controller('posts')
-export class PostsController {}
+@UseInterceptors(SuccessInterceptor)
+@UseFilters(HttpExceptionFilter)
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @Post('free')
+  //@UseGuards(AuthGuard)
+  async createPost(
+    @Body() body: PostRegisterRequestDto,
+  ): Promise<PostRegisterResponseDto> {
+    return await this.postsService.registerPost(body);
+  }
+}
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PostRegisterRequestDto } from './dto/post.request.dto';
+import { PostsRepository } from './posts.repository';
+import { PostRegisterResponseDto } from './dto/post.response.dto';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(private readonly postRepository: PostsRepository) {}
+  async registerPost(
+    data: PostRegisterRequestDto,
+  ): Promise<PostRegisterResponseDto> {
+    return await this.postRepository.create(data);
+  }
+}
