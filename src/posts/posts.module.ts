1c42500e6138c230fdf4f0f17f98536f4486d6fe
HyunSeong <hhs950120@gmail.com>
2023-02-22 23:00:10 +0900

    FEAT: PostsRepository providers에 추가

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -3,10 +3,11 @@ import { FreePost } from './posts.entity';
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostsRepository } from './posts.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([FreePost])],
   controllers: [PostsController],
-  providers: [PostsService],
+  providers: [PostsService, PostsRepository],
 })
 export class PostsModule {}
