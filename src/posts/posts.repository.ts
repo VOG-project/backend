5679ec179abddefc401d93df424b88af0c98dc28
HyunSeong <hhs950120@gmail.com>
2023-02-24 04:04:40 +0900

    FEAT: POST UPDATE, POST DELETE API 생성

--- src/posts/posts.repository.ts
+++ src/posts/posts.repository.ts
@@ -2,8 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { FreePost } from 'src/posts/posts.entity';
 import { Repository } from 'typeorm';
-import { PostRegisterResponseDto } from './dto/post.response.dto';
+import { PostUpdateRequestDto } from './dto/post.request.dto';
 import { PostRegisterRequestDto } from './dto/post.request.dto';
+import {
+  PostRegisterResponseDto,
+  PostDeleteResponseDto,
+  PostUpdateResponseDto,
+} from './dto/post.response.dto';
 
 @Injectable()
 export class PostsRepository {
@@ -12,10 +17,21 @@ export class PostsRepository {
     private readonly freePostRepository: Repository<FreePost>,
   ) {}
 
+  async delete(postId: number): Promise<PostDeleteResponseDto> {
+    const deletedResult = await this.freePostRepository
+      .createQueryBuilder()
+      .delete()
+      .from(FreePost)
+      .where('id = :id', { id: postId })
+      .execute();
+
+    return { deletedCount: deletedResult.affected };
+  }
+
   async create(data: PostRegisterRequestDto): Promise<PostRegisterResponseDto> {
     const { title, content, gameCategory, writerId } = data;
 
-    const insertResult = await this.freePostRepository
+    const insertedResult = await this.freePostRepository
       .createQueryBuilder()
       .insert()
       .into(FreePost)
@@ -29,6 +45,27 @@ export class PostsRepository {
       ])
       .execute();
 
-    return { postId: insertResult.identifiers[0].id };
+    return { postId: insertedResult.identifiers[0].id };
+  }
+
+  async update(
+    data: PostUpdateRequestDto,
+    postId: number,
+  ): Promise<PostUpdateResponseDto> {
+    const { title, content } = data;
+
+    const updatedResult = await this.freePostRepository
+      .createQueryBuilder()
+      .update(FreePost)
+      .set({
+        title,
+        content,
+      })
+      .where('id = :id', { id: postId })
+      .execute();
+
+    console.log(updatedResult);
+
+    return { updatedCount: updatedResult.affected };
   }
 }
