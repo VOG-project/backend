9ee331496a72b3447079898284da81ccfc29dc1b
HyunSeong <hhs950120@gmail.com>
2023-02-27 23:54:50 +0900

    REFACT: 사용하지 않는 패키지 메소드 삭제

--- src/users/dto/users.request.dto.ts
+++ src/users/dto/users.request.dto.ts
@@ -1,6 +1,6 @@
 import { User } from '../users.entity';
 import { ApiProperty, PickType } from '@nestjs/swagger';
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class UserRegisterRequestDto extends PickType(User, [
   'email',
