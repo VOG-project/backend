70f76cd807e1466d97b12c18be18839fa823d0fb
HyunSeong <hhs950120@gmail.com>
2023-02-27 22:58:35 +0900

    FEAT: 비밀번호 변경 API Request DTO 생성

--- src/users/dto/users.register.dto.ts
+++ src/users/dto/users.register.dto.ts
@@ -18,3 +18,21 @@ export class UserUpdateNicknameRequestDto {
   @IsString()
   newNickname: string;
 }
+
+export class UserUpdatePasswordRequestDto {
+  @ApiProperty({
+    example: 'wiq25123',
+    description: '현재 비밀번호',
+  })
+  @IsNotEmpty()
+  @IsString()
+  currentPassword: string;
+
+  @ApiProperty({
+    example: '2tk3j52l',
+    description: '새로운 비밀번호',
+  })
+  @IsNotEmpty()
+  @IsString()
+  newPassword: string;
+}
