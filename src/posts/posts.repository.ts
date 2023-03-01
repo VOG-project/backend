7bec9ed9361ae882481e9a7475efca17a44142f2
HyunSeong <hhs950120@gmail.com>
2023-03-01 21:16:13 +0900

    FEAT: 메소드 재사용할 수 있도록 수정
    - 특정 엔티티에 종속되지 않도록 DataSource에서 쿼리 빌더 생성
    - 목표로 하는 테이블 이름을 파라미터로 받아서 쿼리 수행

--- src/posts/posts.repository.ts
+++ src/posts/posts.repository.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { FreePost } from 'src/posts/posts.entity';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { PostUpdateRequestDto } from './dto/post.request.dto';
 import { PostRegisterRequestDto } from './dto/post.request.dto';
 import {
@@ -15,6 +15,7 @@ export class PostsRepository {
   constructor(
     @InjectRepository(FreePost)
     private readonly postRepository: Repository<FreePost>,
+    private readonly dataSource: DataSource,
   ) {}
 
   async delete(postId: number): Promise<PostDeleteResponseDto> {
@@ -28,13 +29,16 @@ export class PostsRepository {
     return { deletedCount: deletedResult.affected };
   }
 
-  async create(data: PostRegisterRequestDto): Promise<PostRegisterResponseDto> {
+  async create(
+    data: PostRegisterRequestDto,
+    targetEntity: string,
+  ): Promise<PostRegisterResponseDto> {
     const { title, content, gameCategory, writerId } = data;
 
-    const insertedResult = await this.postRepository
+    const insertedResult = await this.dataSource
       .createQueryBuilder()
       .insert()
-      .into(FreePost)
+      .into(targetEntity)
       .values([
         {
           title,
@@ -44,6 +48,20 @@ export class PostsRepository {
         },
       ])
       .execute();
+
+    // const insertedResult = await this.postRepository
+    //   .createQueryBuilder()
+    //   .insert()
+    //   .into(FreePost)
+    //   .values([
+    //     {
+    //       title,
+    //       content,
+    //       gameCategory,
+    //       writerId,
+    //     },
+    //   ])
+    //   .execute();
 
     return { postId: insertedResult.identifiers[0].id };
   }
