facea31708b04e5c02692397fce15208c274fac8
HyunSeong <hhs950120@gmail.com>
2023-03-01 17:05:05 +0900

    FEAT: Swagger 데코레이터 추가 및 http method 수정
    post -> patch

--- src/uploads/uploads.controller.ts
+++ src/uploads/uploads.controller.ts
@@ -2,7 +2,7 @@ import {
   Controller,
   UseFilters,
   UseInterceptors,
-  Post,
+  Patch,
   UploadedFile,
   Param,
 } from '@nestjs/common';
@@ -12,6 +12,7 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { UploadsService } from './uploads.service';
 import { ValidateIdParam } from './validation/uploads.params.validation';
 import { UploadUserProfileImageResponseDto } from './dto/uploads.response.dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('uploads')
 @UseFilters(HttpExceptionFilter)
@@ -19,7 +20,16 @@ import { UploadUserProfileImageResponseDto } from './dto/uploads.response.dto';
 export class UploadsController {
   constructor(private readonly uploadService: UploadsService) {}
 
-  @Post('users/:id')
+  @Patch('users/:id')
+  @ApiOperation({
+    summary: '유저 프로필 사진 수정',
+    tags: ['Uploads']
+  })
+  @ApiResponse({
+    status: 201,
+    description: '유저 프로필 수정 성공',
+    type: UploadUserProfileImageResponseDto,
+  })
   @UseInterceptors(FileInterceptor('image'))
   async uploadUserProfile(
     @UploadedFile() image: Express.Multer.File,
