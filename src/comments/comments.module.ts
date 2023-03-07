3bc4efa6eab36b6e7f53bf570d4a96d728c14c2d
HyunSeong <hhs950120@gmail.com>
2023-03-07 21:05:20 +0900

    FEAT: provider 추가, 엔티티 추가

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommentsController } from './comments.controller';
+import { FreePostComment } from './comments.entity';
+import { CommentsRepository } from './comments.repository';
 import { CommentsService } from './comments.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([FreePostComment])],
   controllers: [CommentsController],
-  providers: [CommentsService]
+  providers: [CommentsService, CommentsRepository],
 })
 export class CommentsModule {}
