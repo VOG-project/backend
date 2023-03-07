c21a3141f89fd2bcc9e056184df07207cf04754c
HyunSeong <hhs950120@gmail.com>
2023-03-07 21:59:50 +0900

    FEAT: 댓글 생성 서비스 생성

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { CommentsRepository } from './comments.repository';
+import { CommentRegisterRequestDto } from './dto/comment.request.dto';
+import { CommentRegisterQueryValidation } from './validations/comment.query.validation';
 
 @Injectable()
-export class CommentsService {}
+export class CommentsService {
+  private BOARD_TYPE;
+
+  constructor(private readonly commentRepository: CommentsRepository) {}
+
+  async registerComment(
+    body: CommentRegisterRequestDto,
+    query: CommentRegisterQueryValidation,
+  ) {
+    const { board, postId } = query;
+    if (board === 'free') {
+      this.BOARD_TYPE = 'freePostComment';
+    } else if (board === 'humor') {
+      this.BOARD_TYPE = 'humorPostCommet';
+    } else if (board === 'championship') {
+      this.BOARD_TYPE = 'championshipPostComment';
+    }
+    return await this.commentRepository.create(body, this.BOARD_TYPE, postId);
+  }
+}
