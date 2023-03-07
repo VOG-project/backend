ac99c5e075153ff56c1d43b38ec6c0b26348c677
HyunSeong <hhs950120@gmail.com>
2023-03-07 20:52:21 +0900

    FEAT: 댓글 등록 쿼리 파라미터 validation 생성

---
+++ src/comments/validations/comment.query.validation.ts
@@ -0,0 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsInt } from 'class-validator';
+
+export class CommentRegisterQueryValidation {
+  @ApiProperty({
+    example: 'free',
+    description: '게시판 카테고리',
+  })
+  board: string;
+
+  @ApiProperty({
+    example: 362,
+    description: '게시물 아이디',
+  })
+  @IsInt()
+  @Type(() => Number)
+  postId: number;
+}
