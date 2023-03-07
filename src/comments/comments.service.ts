ae176a5f03947f5327c1e8f27af0869e0118d219
HyunSeong <hhs950120@gmail.com>
2023-03-08 03:43:27 +0900

    FEAT: 댓글 삭제 서비스 생성

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { CommentsRepository } from './comments.repository';
 import { CommentRegisterRequestDto } from './dto/comment.request.dto';
-import { CommentRegisterResponseDto } from './dto/comment.response.dto';
-import { CommentRegisterQueryDto } from './dto/comment.query.dto';
+import {
+  CommentDeleteResponseDto,
+  CommentRegisterResponseDto,
+} from './dto/comment.response.dto';
+import {
+  CommentDeleteQueryDto,
+  CommentRegisterQueryDto,
+} from './dto/comment.query.dto';
+import { CommentDeleteParamDto } from './dto/comment.param.dto';
 
 @Injectable()
 export class CommentsService {
@@ -25,5 +32,22 @@ export class CommentsService {
     }
 
     return await this.commentRepository.create(body, this.BOARD_TYPE, postId);
+  }
+
+  async deleteComment(
+    data: CommentDeleteParamDto,
+    filter: CommentDeleteQueryDto,
+  ): Promise<CommentDeleteResponseDto> {
+    const { board } = filter;
+
+    if (board === 'free') {
+      this.BOARD_TYPE = 'freePostComment';
+    } else if (board === 'humor') {
+      this.BOARD_TYPE = 'humorPostComment';
+    } else if (board === 'championship') {
+      this.BOARD_TYPE = 'championshipPostComment';
+    }
+
+    return await this.commentRepository.delete(data, this.BOARD_TYPE);
   }
 }
