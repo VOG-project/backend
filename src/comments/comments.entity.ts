9a4134c5b268da5e919f3bea34eb64bb08d5d35a
HyunSeong <hhs950120@gmail.com>
2023-03-07 22:35:46 +0900

    FEAT: 유머 게시판 댓글 엔티티 생성

--- src/comments/comments.entity.ts
+++ src/comments/comments.entity.ts
@@ -1,7 +1,7 @@
 import { Entity, JoinColumn, ManyToOne } from 'typeorm';
 import { CommonCommentEntity } from './../commonEntities/comment.common.entity';
 import { User } from 'src/users/users.entity';
-import { FreePost } from 'src/posts/posts.entity';
+import { FreePost, HumorPost } from 'src/posts/posts.entity';
 
 @Entity({
   name: 'freePostComment',
@@ -17,4 +17,20 @@ export class FreePostComment extends CommonCommentEntity {
   @ManyToOne(() => FreePost, (freePost) => freePost.freePostComment)
   @JoinColumn({ name: 'postId' })
   freePost: FreePost;
+}
+
+@Entity({
+  name: 'humorPostComment',
+})
+export class HumorPostComment extends CommonCommentEntity {
+  @ManyToOne(() => User, (user) => user.humorPostComment, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  @JoinColumn({ name: 'writerId' })
+  user: User;
+
+  @ManyToOne(() => HumorPost, (humorPost) => humorPost.humorPostComment)
+  @JoinColumn({ name: 'postId' })
+  humorPost: HumorPost;
 }
