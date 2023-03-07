346e0ca40f7ddf203cad6b60f1352c9719211365
HyunSeong <hhs950120@gmail.com>
2023-03-07 21:59:21 +0900

    FEAT: 댓글 등록 API 생성

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  UseFilters,
+  UseInterceptors,
+  Post,
+  Body,
+  Query,
+} from '@nestjs/common';
+import { HttpExceptionFilter } from 'src/filters/http-exception.filter';
+import { SuccessInterceptor } from './../interceptors/success.interceptor';
+import { CommentRegisterRequestDto } from './dto/comment.request.dto';
+import { CommentRegisterQueryValidation } from './validations/comment.query.validation';
+import { CommentsService } from './comments.service';
 
 @Controller('comments')
-export class CommentsController {}
+@UseFilters(HttpExceptionFilter)
+@UseInterceptors(SuccessInterceptor)
+export class CommentsController {
+  constructor(private readonly commentService: CommentsService) {}
+
+  @Post()
+  async registerFreePostComment(
+    @Body() body: CommentRegisterRequestDto,
+    @Query() query: CommentRegisterQueryValidation,
+  ) {
+    return await this.commentService.registerComment(body, query);
+  }
+}
