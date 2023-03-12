f9cba12ac5b11454ff71a3aae11bd00dc875b67c
HyunSeong <hhs950120@gmail.com>
2023-03-12 15:33:42 +0900

    FEAT: 대댓글 기능에 필요한 컬럼 추가
    - group: 댓글 그룹을 나누는 컬럼
    - sequence: 댓글의 순서(0은 댓글, 1 이상은 대댓글)

--- src/commonEntities/comment.common.entity.ts
+++ src/commonEntities/comment.common.entity.ts
@@ -38,4 +38,23 @@ export class CommonCommentEntity extends CreatedUpdatedDate {
     length: 500,
   })
   content: string;
+
+  @ApiProperty({
+    example: 6,
+    description: '댓글 그룹(답글이 소속된 댓글 pk)',
+  })
+  @Column({
+    type: 'int',
+  })
+  group: number;
+
+  @ApiProperty({
+    example: 2,
+    description: '댓글의 순서(0은 댓글, 1 이상은 답글)',
+  })
+  @Column({
+    type: 'int',
+    default: 0,
+  })
+  sequence: number;
 }
