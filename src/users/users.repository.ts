317b628fdf169861449f6a8a6c6044a93aba5b96
HyunSeong <hhs950120@gmail.com>
2023-03-19 04:28:06 +0900

    comment: User Repository 주석 추가

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -12,6 +12,12 @@ import { UploadUserProfileImageResponseDto } from './../uploads/dto/uploads.resp
 export class UserRepository {
   constructor(@InjectRepository(User) private userModel: Repository<User>) {}
 
+  /**
+   * User 테이블의 profileUrl 필드를 새로운 이미지 URL로 업데이트하고 업데이트된 row 개수와 이미지 URl을 반환합니다.
+   * @param userId 유저 아이디(PK)
+   * @param fileUrl 유저 프로필 이미지가 저장된 S3 URL
+   * @returns Object {업데이트된 row 개수, 프로필 이미지 URL}
+   */
   async updateProfileUrl(
     userId: number,
     fileUrl: string,
@@ -38,6 +44,12 @@ export class UserRepository {
     }
   }
 
+  /**
+   * User 테이블의 password 필드를 새로운 비밀번호로 업데이트하고 업데이트된 row 개수를 반환합니다.
+   * @param userId 유저 아이디(PK)
+   * @param hashedPassword 해시화된 비밀번호
+   * @returns Object {업데이트된 row 개수}
+   */
   async updatePassword(
     userId: number,
     hashedPassword: string,
@@ -61,6 +73,12 @@ export class UserRepository {
     }
   }
 
+  /**
+   * User 테이블의 nickname 필드를 새로운 닉네임으로 업데이트하고 업데이트된 row 개수를 반환합니다.
+   * @param userId 유저 아이디(PK)
+   * @param newNickname 닉네임
+   * @returns Object {업데이트된 row 개수}
+   */
   async updateNickname(
     userId: number,
     newNickname: string,
@@ -86,6 +104,10 @@ export class UserRepository {
     }
   }
 
+  /**
+   * User 테이블에서 userId에 해당하는 데이터를 반환합니다.
+   * @param userId 유저 아이디(PK)
+   */
   async findById(userId: number) {
     try {
       const user = await this.userModel
@@ -100,6 +122,13 @@ export class UserRepository {
     }
   }
 
+  /**
+   * User 테이블에 새로운 유저의 정보를 등록합니다.
+   * @param email 유저 이메일
+   * @param password 유저 패스워드
+   * @param nickname 유저 닉네임
+   * @param sex 유저 성별
+   */
   async create(
     email: string,
     password: string,
@@ -126,6 +155,11 @@ export class UserRepository {
     }
   }
 
+  /**
+   * User 테이블에서 email로 유저의 데이터를 검색하고 해당 유저 데이터의 모든 컬럼을 반환합니다.
+   * @param email 유저 이메일
+   * @returns 검색된 유저 데이터의 모든 컬럼
+   */
   async findByEmail(email: string) {
     try {
       const user = await this.userModel
@@ -142,6 +176,11 @@ export class UserRepository {
     }
   }
 
+  /**
+   * User 테이블에서 nickname으로 유저의 데이터를 검색하고 해당 유저 데이터의 모든 컬럼을 반환합니다.
+   * @param nickname 유저 닉네임
+   * @returns 검색된 유저 데이터의 모든 컬럼
+   */
   async findByNickname(nickname: string) {
     try {
       const user = await this.userModel
@@ -158,6 +197,11 @@ export class UserRepository {
     }
   }
 
+  /**
+   * User 테이블에서 userId와 일치하는 유저의 데이터(row)를 삭제하고 삭제된 row 개수를 반환합니다.
+   * @param userId 유저 아이디(PK)
+   * @returns 삭제된 row 개수
+   */
   async deleteById(userId: number): Promise<UserDeletedInfoResponseDto> {
     try {
       const deletedResult = await this.userModel
