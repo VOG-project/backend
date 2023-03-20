24f6326faec01d7c3cd1736d218975b0669825d5
HyunSeong <hhs950120@gmail.com>
2023-03-20 21:25:38 +0900

    FEAT: 채팅방 Entity 생성

---
+++ src/chats/chats.entity.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+import { CreatedUpdatedDate } from 'src/commonEntities/date.common.entity';
+
+@Entity()
+export class ChatEntity extends CreatedUpdatedDate {
+  @PrimaryColumn({
+    type: 'varchar',
+    length: 100,
+  })
+  roomId: string;
+
+  @Column({
+    type: 'varchar',
+    length: 30,
+  })
+  title: string;
+
+  @Column({
+    type: 'int',
+    default: 1,
+  })
+  currentMember: number;
+
+  @Column({
+    type: 'int',
+  })
+  maximumMember: number;
+}
