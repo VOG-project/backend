3182db3595d55bd397bf7e4db665fb93c663f44b
HyunSeong <hhs950120@gmail.com>
2023-03-20 21:08:00 +0900

    FEAT: 채팅 API를 위한 컨트롤러, 서비스 생성

--- src/chats/chats.module.ts
+++ src/chats/chats.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ChatsGateway } from './chats.gateway';
+import { ChatsController } from './chats.controller';
+import { ChatsService } from './chats.service';
 
 @Module({
-  providers: [ChatsGateway]
+  providers: [ChatsGateway, ChatsService],
+  controllers: [ChatsController]
 })
 export class ChatsModule {}
