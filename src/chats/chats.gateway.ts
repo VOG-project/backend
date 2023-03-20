7f3a611b380a2a26130b4bf777d230cc1b4b7a1b
HyunSeong <hhs950120@gmail.com>
2023-03-20 20:34:43 +0900

    FEAT: 소켓 연결 및 연결 해제 시 유저의 입퇴장 여부 브로드캐스팅

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -4,19 +4,32 @@ import {
   SubscribeMessage,
   WebSocketGateway,
 } from '@nestjs/websockets';
+import {
+  OnGatewayInit,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets/interfaces';
 import { Socket } from 'socket.io';
 
-@WebSocketGateway()
-export class ChatsGateway {
+@WebSocketGateway(80, { namespace: 'chat' })
+export class ChatsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  handleConnection(@ConnectedSocket() socket: Socket) {
+    console.log(socket.id + '님이 입장하셨습니다.');
+    socket.broadcast.emit('welcomeUser', socket.id + '님이 입장하셨습니다.');
+  }
+
+  handleDisconnect(@ConnectedSocket() socket: Socket) {
+    console.log(socket.id + '님이 퇴장하셨습니다.');
+    socket.broadcast.emit('leaveUser', socket.id + '님이 퇴장하셨습니다.');
+  }
+
   @SubscribeMessage('newUser')
   handleNewUser(
-    @MessageBody() user: string,
+    @MessageBody() body: any,
     @ConnectedSocket() socket: Socket,
   ): string {
-
-    
-    socket.on()
-
     return 'Hello world!';
   }
 }
