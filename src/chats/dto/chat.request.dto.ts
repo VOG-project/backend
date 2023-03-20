e7a118194da808a1383a5a7b2f1f3df2b9c2234d
HyunSeong <hhs950120@gmail.com>
2023-03-20 21:16:57 +0900

    FEAT: 채팅방 생성 API에서 사용될 Request DTO 생성

---
+++ src/chats/dto/chat.request.dto.ts
@@ -0,0 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, IsInt } from 'class-validator';
+import { CreatedUpdatedDate } from 'src/commonEntities/date.common.entity';
+
+export class ChatRegisterRoomRequestDto extends CreatedUpdatedDate {
+  @ApiProperty({
+    example: '미드 정글 두 자리 남습니다.',
+    description: '방 제목',
+  })
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @ApiProperty({
+    example: 5,
+    description: '최대 인원',
+  })
+  @IsNotEmpty()
+  @IsInt()
+  maximum: number;
+}
