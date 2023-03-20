3b137eba814757447c8d95ebcc1e8089a9a4a783
HyunSeong <hhs950120@gmail.com>
2023-03-21 00:00:29 +0900

    FEAT: 채팅방 참여 허용 여부 확인 API를 위한 Param DTO 생성

---
+++ src/chats/dto/chat.param.dto.ts
@@ -0,0 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class ChatAcceptParticipationParamDto {
+  @ApiProperty({
+    example: 'c041d4ef-d7ff-4178-bd82-82dcbefa9a94',
+    description: '참여하고자 하는 채팅방 roomId',
+  })
+  roomId: string;
+}
