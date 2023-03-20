384ef8df485b18c2fd467cb4b8b426102a198f56
HyunSeong <hhs950120@gmail.com>
2023-03-20 21:39:31 +0900

    FEAT: 채팅방 생성 서비스 메소드 생성
    - roomId로 사용될 uuid 생성 후 레파지토리 메소드 호출

--- src/chats/chats.service.ts
+++ src/chats/chats.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { ChatRegisterRoomRequestDto } from './dto/chat.request.dto';
+import { ChatRepository } from './chats.repository';
+import { v4 } from 'uuid';
 
 @Injectable()
-export class ChatsService {}
+export class ChatsService {
+  constructor(private readonly chatRepository: ChatRepository) {}
+
+  async registerChatRoom(data: ChatRegisterRoomRequestDto): Promise<any> {
+    const roomId = v4();
+
+    return await this.chatRepository.create(data, roomId);
+  }
+}
