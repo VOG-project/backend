86d3e0a58cd4cba0732e648c9855e2536ecfac7f
HyunSeong <hhs950120@gmail.com>
2023-03-21 16:19:27 +0900

    FEAT: 채팅 참여 가능 여부를 확인하는 메소드 추가

--- src/chats/chats.service.ts
+++ src/chats/chats.service.ts
@@ -1,8 +1,15 @@
 import { Injectable, HttpException } from '@nestjs/common';
-import { ChatRegisterRoomRequestDto } from './dto/chat.request.dto';
+import {
+  ChatAcceptParticipationRequestDto,
+  ChatRegisterRoomRequestDto,
+} from './dto/chat.request.dto';
 import { ChatsRepository } from './chats.repository';
 import { v4 } from 'uuid';
-import { ChatRegisterRoomResponseDto } from './dto/chat.response.dto';
+import {
+  ChatAcceptParticipationResponseDto,
+  ChatRegisterRoomResponseDto,
+} from './dto/chat.response.dto';
+import { ChatAcceptParticipationParamDto } from './dto/chat.param.dto';
 
 @Injectable()
 export class ChatsService {
@@ -22,5 +29,35 @@ export class ChatsService {
 
     await this.chatRepository.create(data, roomId);
     return await this.chatRepository.findByRoomId(roomId);
+  }
+
+  async acceptParticipation(
+    data: ChatAcceptParticipationRequestDto,
+    identifier: ChatAcceptParticipationParamDto,
+  ): Promise<ChatAcceptParticipationResponseDto> {
+    const { userId } = data;
+    const { roomId } = identifier;
+
+    const isParticipating = await this.chatRepository.existsByUserId(userId);
+
+    if (isParticipating) {
+      throw new HttpException('이미 참여 중인 채팅방이 존재합니다.', 401);
+    }
+
+    const room = await this.chatRepository.findByRoomId(roomId);
+
+    if (!room) {
+      throw new HttpException('해당 채팅방은 존재하지 않습니다.', 400);
+    }
+
+    const { currentMember, maximumMember } = room;
+
+    if (currentMember >= maximumMember) {
+      throw new HttpException('인원이 초과되어 입장할 수 없습니다.', 400);
+    }
+
+    return {
+      canParticipant: true,
+    };
   }
 }
