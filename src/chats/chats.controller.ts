c3220b506f09892cd636f9161b3e919d59eef506
HyunSeong <hhs950120@gmail.com>
2023-03-21 22:05:07 +0900

    FEAT: 채팅방 목록 반환 및 총 개수 반환 API 컨트롤러 생성

--- src/chats/chats.controller.ts
+++ src/chats/chats.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, UseFilters, UseInterceptors, Get } from '@nestjs/common';
-import { Body, Param, Post } from '@nestjs/common/decorators';
+import { Body, Param, Post, Query } from '@nestjs/common/decorators';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { HttpExceptionFilter } from './../filters/http-exception.filter';
 import { SuccessInterceptor } from './../interceptors/success.interceptor';
@@ -10,9 +10,12 @@ import {
 import { ChatsService } from './chats.service';
 import {
   ChatAcceptParticipationResponseDto,
+  ChatGetRoomListResponseDto,
+  ChatGetRoomTotalCountResponseDto,
   ChatRegisterRoomResponseDto,
 } from './dto/chat.response.dto';
 import { ChatAcceptParticipationParamDto } from './dto/chat.param.dto';
+import { ChatGetChatRoomListQueryDto } from './dto/chat.query.dto';
 
 @Controller('chats')
 @UseFilters(HttpExceptionFilter)
@@ -20,7 +23,7 @@ import { ChatAcceptParticipationParamDto } from './dto/chat.param.dto';
 export class ChatsController {
   constructor(private readonly chatService: ChatsService) {}
 
-  @Post('room')
+  @Post('rooms')
   @ApiOperation({
     summary: '채팅방 생성 API',
     tags: ['chats'],
@@ -36,15 +39,47 @@ export class ChatsController {
     return await this.chatService.registerChatRoom(body);
   }
 
-  @Get('room/:roomId')
+  @Post('rooms/:roomId')
   @ApiOperation({
     summary: '채팅방 입장 API',
     tags: ['chats'],
+  })
+  @ApiResponse({
+    description: '채팅방 입장 가능 여부 반환',
+    type: ChatAcceptParticipationResponseDto,
   })
   async acceptParticipation(
     @Param() param: ChatAcceptParticipationParamDto,
     @Body() body: ChatAcceptParticipationRequestDto,
   ): Promise<ChatAcceptParticipationResponseDto> {
     return this.chatService.acceptParticipation(body, param);
+  }
+
+  @Get('rooms')
+  @ApiOperation({
+    summary: '채팅방 목록 반환 API',
+    tags: ['chats'],
+  })
+  @ApiResponse({
+    description: '채팅방 목록 반환',
+    type: ChatGetRoomListResponseDto,
+  })
+  async getChatRoomList(
+    @Query() query: ChatGetChatRoomListQueryDto,
+  ): Promise<ChatGetRoomListResponseDto[]> {
+    return await this.chatService.getChatRoomList(query);
+  }
+
+  @Get('rooms/count')
+  @ApiOperation({
+    summary: '채팅방 총 개수 반환 API',
+    tags: ['chats'],
+  })
+  @ApiResponse({
+    description: '채팅방 총 개수 반환',
+    type: ChatGetRoomTotalCountResponseDto,
+  })
+  async getChatRoomTotalCount(): Promise<ChatGetRoomTotalCountResponseDto> {
+    return await this.chatService.getChatRoomTotalCount();
   }
 }
