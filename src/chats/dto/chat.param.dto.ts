bd0595829164cda50e1022845ecfb3c7b003a543
HyunSeong <hhs950120@gmail.com>
2023-03-21 16:09:14 +0900

    FIX: 클래시 명 변경
    - ChatRegisterParticipantParamDto -> ChatAcceptParticipationParamDto

--- src/chats/dto/chat.param.dto.ts
+++ src/chats/dto/chat.param.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
-export class ChatRegisterParticipantParamDto {
+export class ChatAcceptParticipationParamDto {
   @ApiProperty({
     example: 'c041d4ef-d7ff-4178-bd82-82dcbefa9a94',
     description: '참여하고자 하는 채팅방 roomId',
