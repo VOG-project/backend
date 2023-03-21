332c3a17712a68ff3434f611705a79bbaa746cf6
HyunSeong <hhs950120@gmail.com>
2023-03-21 18:33:29 +0900

    FEAT: 소켓 연결 시 작동될 로직 작성
    - 참여 가능한 채팅방인지 확인
    - 해당 채팅방의 현재 인원 +1 카운트
    - 참여자에 대한 소켓 데이터 등록
    - 채팅방과 채팅방 참여자들에 대한 데이터 반환

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -5,23 +5,50 @@ import {
   WebSocketGateway,
 } from '@nestjs/websockets';
 import {
-  OnGatewayInit,
   OnGatewayConnection,
   OnGatewayDisconnect,
 } from '@nestjs/websockets/interfaces';
 import { Socket } from 'socket.io';
+import { SocketRegisterInfoRequestDto } from './dto/socket.request.dto';
+import { ChatsRepository } from './chats.repository';
+import { ChatsService } from './chats.service';
 
 @WebSocketGateway(80, { namespace: 'chat' })
-export class ChatsGateway
-  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
-{
-  handleConnection(@ConnectedSocket() socket: Socket) {
-    console.log(socket.id + '님이 입장하셨습니다.');
-    socket.broadcast.emit('welcomeUser', socket.id + '님이 입장하셨습니다.');
+export class ChatsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  constructor(
+    private readonly chatService: ChatsService,
+    private readonly chatRepository: ChatsRepository,
+  ) {}
+
+  async handleConnection(
+    @ConnectedSocket() socket: Socket,
+    @MessageBody() body: SocketRegisterInfoRequestDto,
+  ) {
+    const { userId, roomId } = body;
+
+    body.socketId = socket.id;
+
+    const { canParticipant } = await this.chatService.acceptParticipation(
+      { userId },
+      { roomId },
+    );
+
+    if (!canParticipant) {
+      socket.emit('acceptParticipant', '이미 가득 찬 방입니다.');
+      socket.disconnect();
+    }
+
+    this.chatRepository.addMemberCountOne(roomId);
+
+    this.chatRepository.createSocketInfo(body);
+
+    const chatInfo = this.chatRepository.findChatRoomAndParticipantInfo(roomId);
+
+    socket.emit('setChat', chatInfo);
   }
 
   handleDisconnect(@ConnectedSocket() socket: Socket) {
-    console.log(socket.id + '님이 퇴장하셨습니다.');
+    
     socket.broadcast.emit('leaveUser', socket.id + '님이 퇴장하셨습니다.');
   }
 
