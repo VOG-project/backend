f53ed5ced96002578e4940da35718dcce436fa7d
HyunSeong <hhs950120@gmail.com>
2023-03-23 03:02:06 +0900

    FEAT: 게시물 생성 DTO 생성
    - 제목, 내용, 게시판 카테고리를 입력으로 받음

---
+++ src/posts/dto/create.post.dto.ts
@@ -0,0 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class PostRequestDto {
+  @ApiProperty({
+    example: '챌리저 찍음 ㅎㅎ',
+    description: '게시물 제목',
+  })
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @ApiProperty({
+    example: '진짜 찍은 건 아님 ㅋㅋ 껄껄껄',
+    description: '게시물 내용',
+  })
+  @IsNotEmpty()
+  @IsString()
+  content: string;
+
+  @ApiProperty({
+    example: 'free',
+    description: '게시판 카테고리(free | humor | championship',
+  })
+  @IsNotEmpty()
+  @IsString()
+  boardCategory: string;
+}
