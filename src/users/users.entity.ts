9d354d4d243a23bd0c8417e0c5522289c09d5156
HyunSeong <hhs950120@gmail.com>
2023-03-23 02:45:12 +0900

    FIX: 기존 댓글 엔티티와의 관계 해제

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -1,30 +1,8 @@
-import {
-  Entity,
-  Column,
-  PrimaryGeneratedColumn,
-  Check,
-  OneToMany,
-} from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, Check } from 'typeorm';
 import { CreatedUpdatedDate } from 'src/commonEntities/date.common.entity';
 import { IsEmail, IsString, IsInt, IsNotEmpty } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
-import {
-  ChampionshipPostComment,
-  FreePostComment,
-  HumorPostComment,
-} from './../comments/comments.entity';
 
-// 유저 테이블에 대한 엔티티입니다.
-// --
-// CreatedUpdatedDate 엔티티를 상속받아 createdAt, updatedAt 컬럼을 추가합니다.
-// --
-// FreePost 엔티티와 1 : N 관계입니다.
-// HumorPost 엔티티와 1 : N 관계입니다.
-// ChampionPost 엔티티와 1 : N 관계입니다.
-// --
-// FreePostComment 엔티티와 1 : N 관계입니다.
-// HumorPostCommet 엔티티와 1 : N 관계입니다.
-// ChampionPostComment 엔티티와 1 : N 관계입니다.
 @Entity({
   engine: 'InnoDB',
 })
@@ -104,19 +82,4 @@ export class User extends CreatedUpdatedDate {
     default: `https://vog-image-storage.s3.ap-northeast-2.amazonaws.com/user/default.jpg`,
   })
   profileUrl: string;
-
-  @OneToMany(() => FreePostComment, (freePostComment) => freePostComment.user)
-  freePostComment: FreePostComment[];
-
-  @OneToMany(
-    () => HumorPostComment,
-    (humorPostComment) => humorPostComment.user,
-  )
-  humorPostComment: HumorPostComment[];
-
-  @OneToMany(
-    () => ChampionshipPostComment,
-    (championshipPostComment) => championshipPostComment.user,
-  )
-  championshipPostComment: ChampionshipPostComment[];
 }
