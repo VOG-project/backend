d1c83631d19f1f3d4de1eb86542425c14d4bdbdc
HyunSeong <hhs950120@gmail.com>
2023-03-23 03:52:34 +0900

    FEAT: 게시물 생성 메소드 생성

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PostRequestDto } from './dto/create.post.dto';
+import { PostsRepository } from './posts.repository';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(private readonly postRepository: PostsRepository) {}
+
+  registerPost(postRequestDto: PostRequestDto) {
+    return this.postRepository.create(postRequestDto);
+  }
+}
