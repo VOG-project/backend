f89a1088444ba3c753291a9c5eaed9cd72b38908
HyunSeong <hhs950120@gmail.com>
2023-03-23 04:07:05 +0900

    FIX: 잘못 등록한 post 엔티티 삭제

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,10 +4,9 @@ import { UserService } from './users.service';
 import { UserRepository } from './users.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users.entity';
-import { PostEntity } from 'src/posts/posts.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, PostEntity])],
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UserService, UserRepository],
   exports: [UserRepository],
