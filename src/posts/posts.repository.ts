eb131bf7a54a6dcfcfe947264e6c22e07597f791
HyunSeong <hhs950120@gmail.com>
2023-03-23 03:50:36 +0900

    FEAT: 레파지토리 생성 및 게시물 생성 메소드 생성

---
+++ src/posts/posts.repository.ts
@@ -0,0 +1,23 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { PostEntity } from './posts.entity';
+import { Repository } from 'typeorm';
+import { PostRequestDto } from './dto/create.post.dto';
+import { HttpException } from '@nestjs/common';
+
+export class PostsRepository {
+  constructor(
+    @InjectRepository(PostEntity) private readonly post: Repository<PostEntity>,
+  ) {}
+
+  async create(postRequestDto: PostRequestDto): Promise<void> {
+    try {
+      await this.post
+        .createQueryBuilder()
+        .insert()
+        .values(postRequestDto)
+        .execute();
+    } catch (err) {
+      throw new HttpException(`[MYSQL ERROR] create: ${err.message}`, 500);
+    }
+  }
+}
