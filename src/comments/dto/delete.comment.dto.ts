a9b797116090345c2e939edab0e1df34f10fdcd1
HyunSeong <hhs950120@gmail.com>
2023-03-23 23:56:54 +0900

    FEAT: 댓글 삭제 시 사용될 Condition DTO 생성

---
+++ src/comments/dto/delete.comment.dto.ts
@@ -0,0 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsInt } from 'class-validator';
+
+export class CommentDeleteCondition {
+  @ApiProperty({
+    example: 32,
+    description: '댓글 그룹(답글이 소속된 댓글 pk)',
+  })
+  @IsNotEmpty()
+  @IsInt()
+  group: number;
+
+  @ApiProperty({
+    example: 1,
+    description: '댓글의 순서(0은 댓글, 1 이상은 답글)',
+  })
+  sequence: number;
+}
