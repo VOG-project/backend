c0f54996c2c7fbe547955bc5d626037268a34d0a
HyunSeong <hhs950120@gmail.com>
2023-03-23 16:30:38 +0900

    FEAT: 게시물 목록 반환 API 컨트롤러 생성

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -3,12 +3,15 @@ import {
   UseFilters,
   UseInterceptors,
   Post,
+  Get,
   Body,
+  Query,
 } from '@nestjs/common';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { HttpExceptionFilter } from 'src/filters/http-exception.filter';
 import { SuccessInterceptor } from './../interceptors/success.interceptor';
 import { PostRequestDto } from './dto/create.post.dto';
+import { PostGetCondition } from './dto/get.post.dto';
 import { PostEntireResponseDto } from './dto/response.post.dto';
 import { PostsService } from './posts.service';
 
@@ -31,5 +34,18 @@ export class PostsController {
     @Body() postRequestDto: PostRequestDto,
   ): Promise<PostEntireResponseDto> {
     return this.postService.registerPost(postRequestDto);
+  }
+
+  @Get()
+  @ApiOperation({
+    summary: '게시물 목록 조회 API',
+    tags: ['posts'],
+  })
+  @ApiResponse({
+    description:
+      '쿼리 스트링으로 게시판 이름을 전달하면 최신 순으로 10개씩 게시물 목록을 반환합니다.',
+  })
+  getPostList(@Query() condition: PostGetCondition) {
+    return this.postService.getPostList(condition);
   }
 }
