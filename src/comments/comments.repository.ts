891bddf3fd30d2af0e66b3d4239d0d21d0919197
HyunSeong <hhs950120@gmail.com>
2023-03-23 23:11:30 +0900

    FEAT: 댓글 생성 메소드 및 댓글 조회 메소드 생성

--- src/comments/comments.repository.ts
+++ src/comments/comments.repository.ts
@@ -1,10 +1,47 @@
 import { InjectRepository } from '@nestjs/typeorm';
 import { CommentEntity } from './comments.entity';
 import { Repository } from 'typeorm';
+import { CommentCreateRequest } from './dto/create.comment.dto';
+import { HttpException } from '@nestjs/common';
+import {
+  CommentPkIdReturn,
+  CommetEntireDataReturn,
+} from './dto/return.comment.dto';
 
 export class CommentsRepository {
   constructor(
     @InjectRepository(CommentEntity)
     private readonly commentModel: Repository<CommentEntity>,
   ) {}
+
+  async createComment(
+    commentCreateRequest: CommentCreateRequest,
+  ): Promise<CommentPkIdReturn> {
+    try {
+      const insertedComment = await this.commentModel
+        .createQueryBuilder()
+        .insert()
+        .values(commentCreateRequest)
+        .execute();
+
+      return { commentId: insertedComment.identifiers[0].id };
+    } catch (err) {
+      throw new HttpException(
+        `[MYSQL ERROR] createComment: ${err.message}`,
+        500,
+      );
+    }
+  }
+
+  async findOneById(commentId: number): Promise<CommetEntireDataReturn> {
+    try {
+      return await this.commentModel
+        .createQueryBuilder()
+        .select()
+        .where('id = :commentId', { commentId })
+        .getOne();
+    } catch (err) {
+      throw new HttpException(`[MYSQL ERROR] findOneById: ${err.message}`, 500);
+    }
+  }
 }
