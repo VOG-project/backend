3f24e7bae0a63d3790a184968f3d564c5e1260c9
HyunSeong <hhs950120@gmail.com>
2023-03-24 01:35:09 +0900

    FEAT: PostsRepository를 사용하기 위해 Posts 모듈 import

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostsModule } from 'src/posts/posts.module';
 import { CommentsController } from './comments.controller';
 import { CommentEntity } from './comments.entity';
 import { CommentsRepository } from './comments.repository';
 import { CommentsService } from './comments.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CommentEntity])],
+  imports: [TypeOrmModule.forFeature([CommentEntity]), PostsModule],
   controllers: [CommentsController],
   providers: [CommentsService, CommentsRepository],
 })
