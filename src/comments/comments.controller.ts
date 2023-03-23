fe69bb0062da3b8a2498eb23e52ab19fae41fc78
HyunSeong <hhs950120@gmail.com>
2023-03-23 23:11:49 +0900

    FEAT: 댓글 생성 API 컨트롤러 메소드 생성

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,11 +1,19 @@
-import { Controller, UseFilters, UseInterceptors } from '@nestjs/common';
+import { Controller, UseFilters, UseInterceptors, Body } from '@nestjs/common';
 import { SuccessInterceptor } from 'src/interceptors/success.interceptor';
 import { HttpExceptionFilter } from './../filters/http-exception.filter';
 import { CommentsService } from './comments.service';
+import { CommentCreateRequest } from './dto/create.comment.dto';
+import { CommetEntireDataReturn } from './dto/return.comment.dto';
 
 @Controller('comments')
 @UseFilters(HttpExceptionFilter)
 @UseInterceptors(SuccessInterceptor)
 export class CommentsController {
   constructor(private readonly commentService: CommentsService) {}
+
+  registerComment(
+    @Body() commentCreateRequest: CommentCreateRequest,
+  ): Promise<CommetEntireDataReturn> {
+    return this.commentService.registerComment(commentCreateRequest);
+  }
 }
