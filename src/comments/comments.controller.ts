13424c927cd7c6fe322ee5f5aeaacfd015f5eea6
HyunSeong <hhs950120@gmail.com>
2023-03-24 01:28:11 +0900

    FEAT: 댓글 삭제 API 컨트롤러 생성

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -4,13 +4,21 @@ import {
   UseInterceptors,
   Body,
   Post,
+  Delete,
+  Param,
+  ParseIntPipe,
+  Query,
 } from '@nestjs/common';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { SuccessInterceptor } from 'src/interceptors/success.interceptor';
 import { HttpExceptionFilter } from './../filters/http-exception.filter';
 import { CommentsService } from './comments.service';
 import { CommentCreateRequest } from './dto/create.comment.dto';
-import { CommetEntireDataReturn } from './dto/return.comment.dto';
+import { CommentDeleteCondition } from './dto/delete.comment.dto';
+import {
+  CommentDeletedCountReturn,
+  CommetEntireDataReturn,
+} from './dto/return.comment.dto';
 
 @Controller('comments')
 @UseFilters(HttpExceptionFilter)
@@ -31,5 +39,21 @@ export class CommentsController {
     @Body() commentCreateRequest: CommentCreateRequest,
   ): Promise<CommetEntireDataReturn> {
     return this.commentService.registerComment(commentCreateRequest);
+  }
+
+  @Delete()
+  @ApiOperation({
+    summary: '댓글 삭제 API',
+    tags: ['comments'],
+  })
+  @ApiResponse({
+    description: '조건(쿼리)에 해당하는 댓글을 삭제합니다.',
+    type: CommentDeletedCountReturn,
+  })
+  removeComment(
+    @Param('commentId', ParseIntPipe) commentId,
+    @Query() condition: CommentDeleteCondition,
+  ): Promise<CommentDeletedCountReturn> {
+    return this.commentService.removeComment(commentId, condition);
   }
 }
