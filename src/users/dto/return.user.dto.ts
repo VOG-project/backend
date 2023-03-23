43360a60e09f6873b2644257d4981a1a26d15387
HyunSeong <hhs950120@gmail.com>
2023-03-23 20:28:12 +0900

    FEAT: 유저 데이터 전체 반환 Return DTO 생성

---
+++ src/users/dto/return.user.dto.ts
@@ -0,0 +1,54 @@
+import { ApiProperty } from '@nestjs/swagger';
+export class UserEntireDataReturn {
+  @ApiProperty({
+    example: '35',
+    description: '식별아이디',
+  })
+  id: number;
+
+  @ApiProperty({
+    example: 'test10@naver.com',
+    description: '이메일',
+    required: true,
+  })
+  email: string;
+
+  @ApiProperty({
+    example: 'efo234a08sef',
+    description: '비밀번호',
+    required: true,
+  })
+  password?: string;
+
+  @ApiProperty({
+    example: '네스트좋아',
+    description: '닉네임',
+    required: true,
+  })
+  nickname: string;
+
+  @ApiProperty({
+    example: '남',
+    description: '성별',
+    required: true,
+  })
+  sex: string;
+
+  @ApiProperty({
+    example: 'https://www.vog-storage/user/efsef.jpg',
+    description: '유저 프로필 사진 URL',
+  })
+  profileUrl: string;
+
+  @ApiProperty({
+    example: '2023-03-05 16:25:04.871850',
+    description: '게시물 생성 일자',
+  })
+  createdAt: Date;
+
+  @ApiProperty({
+    example: '2023-03-05 16:25:04.871850',
+    description: '게시물 수정 일자',
+  })
+  updatedAt: Date;
+}
