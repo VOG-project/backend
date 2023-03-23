e0adffbeb47d28ac912a56b686e256576e7d4f89
HyunSeong <hhs950120@gmail.com>
2023-03-23 23:27:13 +0900

    FEAT: API DOCS에 추가

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, UseFilters, UseInterceptors, Body } from '@nestjs/common';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { SuccessInterceptor } from 'src/interceptors/success.interceptor';
 import { HttpExceptionFilter } from './../filters/http-exception.filter';
 import { CommentsService } from './comments.service';
@@ -11,6 +12,15 @@ import { CommetEntireDataReturn } from './dto/return.comment.dto';
 export class CommentsController {
   constructor(private readonly commentService: CommentsService) {}
 
+  @Body()
+  @ApiOperation({
+    summary: '댓글 생성 API',
+    tags: ['comments'],
+  })
+  @ApiResponse({
+    description: '댓글 정보를 등록하면 등록된 댓글 데이터를 반환합니다.',
+    type: CommetEntireDataReturn,
+  })
   registerComment(
     @Body() commentCreateRequest: CommentCreateRequest,
   ): Promise<CommetEntireDataReturn> {
