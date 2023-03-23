fcfbe8a6fe3033e262b09a40a71bca299d2f80be
HyunSeong <hhs950120@gmail.com>
2023-03-23 23:11:38 +0900

    FEAT: 댓글 생성 메소드 생성

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,7 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CommentsRepository } from './comments.repository';
+import { CommentCreateRequest } from './dto/create.comment.dto';
+import { CommetEntireDataReturn } from './dto/return.comment.dto';
 
 @Injectable()
 export class CommentsService {
   constructor(private readonly commentRepository: CommentsRepository) {}
+
+  async registerComment(
+    commentCreateRequest: CommentCreateRequest,
+  ): Promise<CommetEntireDataReturn> {
+    const { commentId } = await this.commentRepository.createComment(
+      commentCreateRequest,
+    );
+    return await this.commentRepository.findOneById(commentId);
+  }
 }
