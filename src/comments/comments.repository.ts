865d59b02f68f76803cc9abc60ac686009c4fe64
HyunSeong <hhs950120@gmail.com>
2023-03-23 22:30:05 +0900

    FEAT: 레파지토리에 typeORM Repository 객체 주입

--- src/comments/comments.repository.ts
+++ src/comments/comments.repository.ts
@@ -1,3 +1,10 @@
 import { InjectRepository } from '@nestjs/typeorm';
+import { CommentEntity } from './comments.entity';
+import { Repository } from 'typeorm';
 
-export class CommentsRepository {}
+export class CommentsRepository {
+  constructor(
+    @InjectRepository(CommentEntity)
+    private readonly commentModel: Repository<CommentEntity>,
+  ) {}
+}
