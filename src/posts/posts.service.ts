feb9cc8d12e6e1feefacf26e16ad6227493e8e03
HyunSeong <hhs950120@gmail.com>
2023-03-23 16:30:16 +0900

    FEAT: 게시물 리스트 불러오는 메소드 생성
    - 반환 되는 row 개수는 10개로 고정

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PostRequestDto } from './dto/create.post.dto';
+import { PostGetCondition } from './dto/get.post.dto';
 import { PostEntireResponseDto } from './dto/response.post.dto';
 import { PostsRepository } from './posts.repository';
 
@@ -12,5 +13,19 @@ export class PostsService {
   ): Promise<PostEntireResponseDto> {
     const { postId } = await this.postRepository.create(postRequestDto);
     return this.postRepository.findOneById(postId);
+  }
+
+  async getPostList(condition: PostGetCondition) {
+    let { board } = condition;
+    const { page } = condition;
+
+    const RETURN_ROW_COUNT = 10;
+    board = board.toLowerCase();
+
+    return this.postRepository.findPostListByBoardType(
+      board,
+      page,
+      RETURN_ROW_COUNT,
+    );
   }
 }
