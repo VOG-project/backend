678f27d91fa08665d08a40d424f1fbb7958386ab
HyunSeong <hhs950120@gmail.com>
2023-03-23 21:50:22 +0900

    FIX: API 문서 수정 및 반환 DTO 교체

--- src/uploads/uploads.controller.ts
+++ src/uploads/uploads.controller.ts
@@ -11,8 +11,8 @@ import { HttpExceptionFilter } from 'src/filters/http-exception.filter';
 import { SuccessInterceptor } from './../interceptors/success.interceptor';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { UploadsService } from './uploads.service';
-import { UploadUserProfileImageResponseDto } from './dto/uploads.response.dto';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { UserEntireDataReturn } from 'src/users/dto/return.user.dto';
 
 @Controller('uploads')
 @UseFilters(HttpExceptionFilter)
@@ -22,19 +22,20 @@ export class UploadsController {
 
   @Patch('users/:userId')
   @ApiOperation({
-    summary: '유저 프로필 사진 수정',
+    summary: '유저 프로필 사진 수정 API',
     tags: ['Uploads'],
   })
   @ApiResponse({
     status: 201,
-    description: '유저 프로필 수정 성공',
-    type: UploadUserProfileImageResponseDto,
+    description:
+      '유저 프로필 사진과 유저 아이디를 입력받고 변경된 유저의 데이터를 반환합니다.',
+    type: UserEntireDataReturn,
   })
   @UseInterceptors(FileInterceptor('image'))
   async uploadUserProfile(
     @UploadedFile() image: Express.Multer.File,
     @Param('userId', ParseIntPipe) userId: number,
-  ): Promise<UploadUserProfileImageResponseDto> {
+  ): Promise<UserEntireDataReturn> {
     return this.uploadService.uploadUserProfileImageFile(image, userId);
   }
 }
