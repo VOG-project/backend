5e444cd94419f4a1b88a20ecaee982642b772ced
HyunSeong <hhs950120@gmail.com>
2023-03-24 18:41:08 +0900

    FEAT: 채팅방 조회 condition DTO 생성

--- src/chats/dto/get.chat.dto.ts
+++ src/chats/dto/get.chat.dto.ts
@@ -1,5 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsNumber } from 'class-validator';
+import { IsNotEmpty, IsInt } from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class ChatIsAcceptableCondition {
   @ApiProperty({
@@ -7,6 +8,18 @@ export class ChatIsAcceptableCondition {
     description: '채팅 참여를 원하는 유저 pk',
   })
   @IsNotEmpty()
-  @IsNumber()
+  @IsInt()
+  @Type(() => Number)
   userId: number;
+}
+
+export class ChatChatRoomListCondition {
+  @ApiProperty({
+    example: 2,
+    description: '조회할 채팅방 목록 페이지 번호',
+  })
+  @IsNotEmpty()
+  @IsInt()
+  @Type(() => Number)
+  page: number;
 }
