0644bdd7503af85c260b45b963326b1d3f0ec926
HyunSeong <hhs950120@gmail.com>
2023-03-24 16:41:43 +0900

    FEAT: 채팅방 생성 Request DTO 생성 채팅방 데이터 전체 컬럼 return DTO 생성

---
+++ src/chats/dto/create.chat.dto.ts
@@ -0,0 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsInt, IsString } from 'class-validator';
+
+export class ChatCreateRequest {
+  @ApiProperty({
+    example: 265,
+    description: '채팅방 생성 유저 아이디(PK)',
+  })
+  @IsNotEmpty()
+  @IsInt()
+  userId: number;
+
+  @ApiProperty({
+    example: '미드 정글 두 자리 남습니다.',
+    description: '방 제목',
+  })
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @ApiProperty({
+    example: 5,
+    description: '최대 인원',
+  })
+  @IsNotEmpty()
+  @IsInt()
+  maximumMember: number;
+}
---
+++ src/chats/dto/return.chat.dto.ts
@@ -0,0 +1,45 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class ChatEntireDataReturn {
+  @ApiProperty({
+    example: 'efs3f23a4-362efas-12was',
+    description: '소켓 통신에서 사용될 roomId(uuid)',
+  })
+  roomId: string;
+
+  @ApiProperty({
+    example: '플레 이상 미드 한 분 구함',
+    description: '방 제목',
+  })
+  title: string;
+
+  @ApiProperty({
+    example: 3,
+    description: '현재 인원',
+  })
+  currentMember: number;
+
+  @ApiProperty({
+    example: 6,
+    description: '최대 인원',
+  })
+  maximumMember: number;
+
+  @ApiProperty({
+    example: 2,
+    description: '채팅방 번호',
+  })
+  no: number;
+
+  @ApiProperty({
+    example: '2023-03-05 16:25:04.871850',
+    description: '채팅방 생성 일자',
+  })
+  createdAt: Date;
+
+  @ApiProperty({
+    example: '2023-03-05 16:25:04.871850',
+    description: '채팅방 수정 일자',
+  })
+  updatedAt: Date;
+}
