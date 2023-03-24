d44297f09c4c3ac4b316b3c95e15b3f262f744fd
HyunSeong <hhs950120@gmail.com>
2023-03-24 23:26:51 +0900

    FEAT: REDIS 커넥션 타임아웃 10초로 조정

--- src/app.module.ts
+++ src/app.module.ts
@@ -40,6 +40,7 @@ import { CommentEntity } from './comments/comments.entity';
         host: process.env.REDIS_HOST,
         port: parseInt(process.env.REDIS_PORT, 10),
         password: process.env.REDIS_PASSWORD,
+        connectTimeout: 10000,
       },
     }),
     UsersModule,
