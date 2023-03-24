c8e077e428f1ede68824570c5c4dbdf74086093f
HyunSeong <hhs950120@gmail.com>
2023-03-24 18:20:29 +0900

    FEAT: Validation 추가

--- src/chats/dto/get.chat.dto.ts
+++ src/chats/dto/get.chat.dto.ts
@@ -1,9 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber } from 'class-validator';
 
 export class ChatIsAcceptableCondition {
   @ApiProperty({
     example: 61,
     description: '채팅 참여를 원하는 유저 pk',
   })
+  @IsNotEmpty()
+  @IsNumber()
   userId: number;
 }
