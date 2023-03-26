d6f920d923d39f047b5ef5311ba86b2eb522cd19
HyunSeong <hhs950120@gmail.com>
2023-03-26 17:40:53 +0900

    FEAT: 게시물과 댓글 반환하는 DTO 생성

--- src/posts/dto/return.post.dto.ts
+++ src/posts/dto/return.post.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { CommentEntireDataReturn } from 'src/comments/dto/return.comment.dto';
 
 export class PostEntireDataReturn {
   @ApiProperty({
@@ -86,4 +87,62 @@ export class PostDeletedCountReturn {
     description: '삭제된 게시물 row 개수',
   })
   deletedCount: number;
+}
+
+export class PostAndCommentsReturn {
+  @ApiProperty({
+    example: 6,
+    description: '게시물 PK',
+  })
+  id: number;
+
+  @ApiProperty({
+    example: '챌리저 찍음 ㅎㅎ',
+    description: '게시물 제목',
+  })
+  title: string;
+
+  @ApiProperty({
+    example: '진짜 찍은 건 아님 ㅋㅋ 껄껄껄',
+    description: '게시물 내용',
+  })
+  content: string;
+
+  @ApiProperty({
+    example: 712,
+    description: '좋아요 개수',
+  })
+  likeCount: number;
+
+  @ApiProperty({
+    example: 'free',
+    description: '게시판 카테고리(free | humor | championship)',
+  })
+  postCategory: string;
+
+  @ApiProperty({
+    example: '2023-03-05 16:25:04.871850',
+    description: '게시물 생성 일자',
+  })
+  createdAt: Date;
+
+  @ApiProperty({
+    example: '2023-03-05 16:25:04.871850',
+    description: '게시물 수정 일자',
+  })
+  updatedAt: Date;
+
+  @ApiProperty({
+    example: {
+      id: 6,
+      userId: 7,
+      content: '댓글썼어용',
+      group: 6,
+      sequence: 0,
+      createdAt: '2023-03-25T22:24:18.479Z',
+      updatedAt: '2023-03-25T22:24:18.479Z',
+      childeComments: { id: 7, comment: 2 },
+    },
+  })
+  comments: CommentEntireDataReturn[];
 }
