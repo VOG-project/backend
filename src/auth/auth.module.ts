554063aa9e7de905784e20861c3835b343d8f9f8
HyunSeong <hhs950120@gmail.com>
2023-03-27 21:28:26 +0900

    FEAT: PostsModule 주입

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from './../users/users.module';
+import { PostsModule } from 'src/posts/posts.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, PostsModule],
   providers: [AuthService],
   controllers: [AuthController],
 })
