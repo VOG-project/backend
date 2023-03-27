df8177134a3f639ef50c78d1d60e8c352b55f410
HyunSeong <hhs950120@gmail.com>
2023-03-27 23:22:45 +0900

    FEAT: 좋아요 등록 서비스 메소드 생성

--- src/like/like.service.ts
+++ src/like/like.service.ts
@@ -1,8 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException } from '@nestjs/common';
+import { PostsRepository } from 'src/posts/posts.repository';
+import { UserRepository } from 'src/users/users.repository';
+import { LikeUserReturn } from './dto/result.like.dto';
 import { LikeRepository } from './like.repository';
 
 @Injectable()
 export class LikeService {
-  constructor(private readonly likeRepository: LikeRepository) {}
-  
+  constructor(
+    private readonly likeRepository: LikeRepository,
+    private readonly userRepository: UserRepository,
+    private readonly postRepository: PostsRepository,
+  ) {}
+
+  async registerLike(postId: number, userId: number): Promise<any> {
+    const isExistedUser = await this.userRepository.findOneById(userId);
+    if (!isExistedUser)
+      throw new HttpException('존재하지 않는 유저입니다.', 400);
+
+    const isExistedPost = await this.postRepository.findOneById(postId);
+    if (!isExistedPost)
+      throw new HttpException('존재하지 않는 게시물입니다.', 400);
+
+    await this.likeRepository.createLike(postId, userId);
+
+    const users = await this.likeRepository.findLikeUserByPostId(postId);
+    const result = users.map((id: string) => parseInt(id, 10));
+
+    return result;
+  }
 }
