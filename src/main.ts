7d2d6abd4e86b68e021efd7a2c2a210fc22bb5f8
HyunSeong <hhs950120@gmail.com>
2023-03-28 01:53:04 +0900

    FIX: CORS origin 수정

--- src/main.ts
+++ src/main.ts
@@ -21,13 +21,13 @@ async function bootstrap() {
   );
   app.useGlobalFilters(new HttpExceptionFilter());
   app.use(cookieParser());
-  // app.enableCors({
-  //   origin: '*',
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  //   preflightContinue: false,
-  //   optionsSuccessStatus: 204,
-  //   credentials: true,
-  // });
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://localhost:3002'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('VOG API DOCS')
