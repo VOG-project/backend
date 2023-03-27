52323e18ee692a6e2ff17be75c59566703237112
HyunSeong <hhs950120@gmail.com>
2023-03-27 21:25:10 +0900

    FEAT: UserModule 주입

--- src/like/like.module.ts
+++ src/like/like.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from 'src/users/users.module';
 import { LikeController } from './like.controller';
+import { LikeRepository } from './like.repository';
 import { LikeService } from './like.service';
 
 @Module({
+  imports: [UsersModule],
   controllers: [LikeController],
-  providers: [LikeService]
+  providers: [LikeService, LikeRepository],
 })
 export class LikeModule {}
