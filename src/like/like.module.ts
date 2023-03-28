1fb6efc3b84395c75c2e643cb7e29d24ca3fbde2
HyunSeong <hhs950120@gmail.com>
2023-03-28 16:44:07 +0900

    FEAT: 순환 모듈 종속성 문제 해결

--- src/like/like.module.ts
+++ src/like/like.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { PostsModule } from 'src/posts/posts.module';
 import { UsersModule } from 'src/users/users.module';
 import { LikeController } from './like.controller';
@@ -6,8 +6,9 @@ import { LikeRepository } from './like.repository';
 import { LikeService } from './like.service';
 
 @Module({
-  imports: [UsersModule, PostsModule],
+  imports: [UsersModule, forwardRef(() => PostsModule)],
   controllers: [LikeController],
   providers: [LikeService, LikeRepository],
+  exports: [LikeRepository],
 })
 export class LikeModule {}
--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,12 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
 import { PostsRepository } from './posts.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostEntity } from './posts.entity';
+import { LikeModule } from 'src/like/like.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PostEntity])],
+  imports: [
+    TypeOrmModule.forFeature([PostEntity]),
+    forwardRef(() => LikeModule),
+  ],
   providers: [PostsService, PostsRepository],
   controllers: [PostsController],
   exports: [PostsRepository],
