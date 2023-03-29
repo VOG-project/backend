b1b71383fce1d33d62c0e2a03535be414cac7d7a
HyunSeong <hhs950120@gmail.com>
2023-03-29 16:16:01 +0900

    FEAT: 친구로 등록된 유저 데이터 반환하는 DTO 생성

--- src/friend/friend.repository.ts
+++ src/friend/friend.repository.ts
@@ -2,6 +2,7 @@ import { Injectable, HttpException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { FriendEntity } from './friend.entity';
 import { Repository } from 'typeorm';
+import { FriendFollowingReturn } from './dto/return.friend.dto';
 
 @Injectable()
 export class FriendRepository {
@@ -22,6 +23,28 @@ export class FriendRepository {
         .execute();
     } catch (err) {
       throw new HttpException(`[MYSQL ERROR] create: ${err.message}`, 500);
+    }
+  }
+
+  async findFreindsByUserId(userId: number): Promise<FriendFollowingReturn[]> {
+    try {
+      return await this.friendModel
+        .createQueryBuilder('f')
+        .innerJoin('f.following', 'u')
+        .select([
+          'f.userId',
+          'u.id',
+          'u.email',
+          'u.nickname',
+          'u.sex',
+          'u.profileUrl',
+          'u.createdAt',
+          'u.updatedAt',
+        ])
+        .where('f.userId = :userId', { userId })
+        .getMany();
+    } catch (err) {
+      throw new HttpException(`[MYSQL ERROR] findMany: ${err.message}`, 500);
     }
   }
 }
