a4c9ae596226fa70a26b70b8d4a5152a98845f72
HyunSeong <hhs950120@gmail.com>
2023-03-29 15:30:18 +0900

    FEAT: 친구 데이터 생성 메소드 추가

--- src/friend/friend.repository.ts
+++ src/friend/friend.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { FriendEntity } from './friend.entity';
 import { Repository } from 'typeorm';
@@ -9,4 +9,19 @@ export class FriendRepository {
     @InjectRepository(FriendEntity)
     private readonly friendModel: Repository<FriendEntity>,
   ) {}
+
+  async create(userId: number, targetId: number): Promise<void> {
+    try {
+      await this.friendModel
+        .createQueryBuilder()
+        .insert()
+        .values({
+          userId,
+          targetId,
+        })
+        .execute();
+    } catch (err) {
+      throw new HttpException(`[MYSQL ERROR] create: ${err.message}`, 500);
+    }
+  }
 }
