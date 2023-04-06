3834b160a83c668b87c572547e3e7594dc230ce9
HyunSeong <hhs950120@gmail.com>
2023-04-06 23:14:11 +0900

    FIX

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,12 @@ async function bootstrap() {
   app.useGlobalFilters(new HttpExceptionFilter());
   app.use(cookieParser());
   app.enableCors({
-    origin: process.env.FRONT_END_SERVER_URL,
+    origin: [
+      process.env.FRONT_END_SERVER_URL,
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'http://localhost:3002',
+    ],
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
     credentials: true,
     optionsSuccessStatus: 204,
