fae35772949587851287b9649175498271ef86d4
HyunSeong <hhs950120@gmail.com>
2023-04-10 00:27:21 +0900

    FIX: swagger 관련 데코레이터 삭제

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -15,76 +15,37 @@ import { FriendEntity } from 'src/friend/friend.entity';
 @Entity({
   name: 'user',
 })
-@Check(`"sex IN ("남", "여")"`)
 export class UserEntity extends CreatedUpdatedDate {
-  @ApiProperty({
-    example: '35',
-    description: '식별아이디',
-  })
   @PrimaryGeneratedColumn()
-  @IsInt()
-  @IsNotEmpty()
   id: number;
 
-  @ApiProperty({
-    example: 'test10@naver.com',
-    description: '이메일',
-    required: true,
-  })
   @Column({
     type: 'varchar',
     length: 20,
     unique: true,
   })
-  @IsEmail()
-  @IsNotEmpty()
   email: string;
 
-  @ApiProperty({
-    example: 'efo234a08sef',
-    description: '비밀번호',
-    required: true,
-  })
   @Column({
     type: 'varchar',
     length: 100,
     unique: true,
   })
-  @IsString()
-  @IsNotEmpty()
   password: string;
 
-  @ApiProperty({
-    example: '네스트좋아',
-    description: '닉네임',
-    required: true,
-  })
   @Column({
     type: 'varchar',
     length: 10,
     unique: true,
   })
-  @IsString()
-  @IsNotEmpty()
   nickname: string;
 
-  @ApiProperty({
-    example: '남',
-    description: '성별',
-    required: true,
-  })
   @Column({
     type: 'char',
     length: 2,
   })
-  @IsString()
-  @IsNotEmpty()
   sex: string;
 
-  @ApiProperty({
-    example: 'https://www.vog-storage/user/efsef.jpg',
-    description: '유저 프로필 사진 URL',
-  })
   @Column({
     type: 'varchar',
     length: 120,
