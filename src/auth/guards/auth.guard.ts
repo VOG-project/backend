88364aaa5b472cb01dcef174cb2c9fc9dd578e24
HyunSeong <hhs950120@gmail.com>
2023-04-12 23:54:58 +0900

    FEAT: 인가를 위한 authGuard 생성

--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -5,38 +5,42 @@ import {
   HttpException,
   UseFilters,
 } from '@nestjs/common';
-import { Observable } from 'rxjs';
+//import { Observable } from 'rxjs';
 import { RedisService } from '@liaoliaots/nestjs-redis';
 import Redis from 'ioredis';
 import { HttpExceptionFilter } from 'src/common/filters/http-exception.filter';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 @UseFilters(HttpExceptionFilter)
 export class AuthGuard implements CanActivate {
   private readonly redis: Redis;
 
-  constructor(private readonly redisService: RedisService) {
+  constructor(
+    private readonly redisService: RedisService,
+    private readonly jwtService: JwtService,
+  ) {
     this.redis = this.redisService.getClient('session');
   }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
-    const cookie = request.headers.cookie;
-    console.log('쿠키가 있냐?' + cookie);
-    // 쿠키에 세션 아이디가 없을 때
-    if (!cookie) throw new HttpException('로그인이 필요한 서비스입니다.', 401);
-
-    const sessionId = cookie.split('=')[1];
-    console.log(sessionId);
-    //const isExistedSession = await this.redis.hget(sessionId, 'id');
-    const isExistedSession = await this.redis.exists(sessionId);
-
-    // 쿠키에 세션 아이디는 있으나, DB에 존재하지 않을 때
-    if (!isExistedSession)
+    const authorization = request.headers.authorization;
+    if (!authorization)
       throw new HttpException(
-        '유효하지 않는 세션 아이디입니다. 다시 로그인하세요.',
+        '로그인 하지 않아 토큰이 존재하지 않습니다.',
         401,
       );
+
+    const accessToken = authorization.split(' ')[1];
+    try {
+      const isRightToken = this.jwtService.verify(accessToken, {
+        secret: process.env.JWT_SECRET,
+      });
+      console.log(isRightToken);
+    } catch (err) {
+      throw new HttpException('변조되었거나 만료기간이 지난 토큰입니다.', 400);
+    }
 
     return true;
   }
