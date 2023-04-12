97854a41a9a1c074f9778e99589df2d9fa6a7c7c
HyunSeong <hhs950120@gmail.com>
2023-04-12 17:24:29 +0900

    FEAT: ACcessToken, UserData 받아오는 메소드 추가

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -20,12 +20,32 @@ export class AuthService {
 
   async requestNaverAccessToken(callbackData: AuthAuthorizedCallbackCondition) {
     const { code, state } = callbackData;
+    if (state !== process.env.OAUTH_NAVER_STATE)
+      throw new HttpException(
+        'state 값이 일치하지 않습니다. CSRF 공격 위험이 있습니다.',
+        400,
+      );
+
     const responseData = await axios({
       method: 'get',
-      url: `https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=CVOrbtbzfrKewqSVyirz&client_secret=UOKpr_rHVs&code=${code}&state=${state}`,
+      url: `https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=${process.env.OAUTH_NAVER_CLIENT_ID}&client_secret=${process.env.OAUTH_NAVER_CLIENT_SECRET}&code=${code}&state=${state}`,
     });
 
-    return await this.requestNaverUserData(responseData.data);
+    const oauthId = await this.requestNaverUserData(responseData.data);
+    const user = await this.userRepository.findOneByOAuthId(
+      'tfSb35YY9N_bZgbfSW1jDYkjCcgKrEEHUQ8CLTnmxZM',
+    );
+
+    if (!user) {
+      return {
+        oauthId,
+        message:
+          'oauthId는 발급되었지만 해당하는 유저 데이터가 없습니다. 유저 데이터 입력 창으로 리다이렉트 해주세요',
+        redirectUrl: 'https://talkgg.online/sign-up',
+      };
+    } else {
+      return this.generateJwtAcessToken();
+    }
   }
 
   async requestNaverUserData({ access_token }) {
@@ -37,7 +57,15 @@ export class AuthService {
       url: `https://openapi.naver.com/v1/nid/me`,
     });
 
-    return responseData.data;
+    const oauthId = responseData.data.response.id;
+    if (!oauthId)
+      throw new HttpException('Naver oauthId를 받지 못했습니다.', 400);
+
+    return oauthId;
+  }
+
+  async generateJwtAcessToken() {
+    return 'jwt 토큰';
   }
 
   async deleteSessionInformation(
