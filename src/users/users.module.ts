5e21bc8c2506fbf56b1849eb7c15b36ee3c38bcc
HyunSeong <hhs950120@gmail.com>
2023-04-12 21:30:48 +0900

    FIX: UserModule과 AuthModule 순환 참조 해결

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UserService } from './users.service';
 import { UserRepository } from './users.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './users.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [
+    TypeOrmModule.forFeature([UserEntity]),
+    forwardRef(() => AuthModule),
+  ],
   controllers: [UsersController],
   providers: [UserService, UserRepository],
   exports: [UserRepository],
