6c3c8bdca4ceab86fac6439b7c4660f66f3bfcd6
HyunSeong <hhs950120@gmail.com>
2023-04-12 21:30:28 +0900

    FIX: JwtModule 추가 위치 수정

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from './../users/users.module';
@@ -6,17 +6,9 @@ import { AuthRepository } from './auth.repository';
 import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [
-    UsersModule,
-    JwtModule.register({
-      global: true,
-      secret: process.env.JWT_SECRET,
-      signOptions: {
-        expiresIn: '7d',
-      },
-    }),
-  ],
+  imports: [forwardRef(() => UsersModule)],
   providers: [AuthService, AuthRepository],
   controllers: [AuthController],
+  exports: [AuthService],
 })
 export class AuthModule {}
