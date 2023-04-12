401940a80b643320e613f893facae0dc80e43db1
HyunSeong <hhs950120@gmail.com>
2023-04-12 23:55:11 +0900

    FEAT: 테스트 용 UseGuards 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,14 @@
-import { Controller, UseFilters, UseInterceptors, Get } from '@nestjs/common';
+import {
+  Controller,
+  UseGuards,
+  UseFilters,
+  UseInterceptors,
+  Get,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { SuccessInterceptor } from './common/interceptors/success.interceptor';
+import { AuthGuard } from './auth/guards/auth.guard';
 
 @Controller()
 @UseFilters(HttpExceptionFilter)
@@ -10,6 +17,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('test')
+  @UseGuards(AuthGuard)
   abc() {
     return 'abcdefg';
   }
