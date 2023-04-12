b0e1bc6a7ef7145618107f0fd20b5db954bc8f2b
HyunSeong <hhs950120@gmail.com>
2023-04-12 17:32:08 +0900

    FEAT: Jwt 모듈 추가

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,19 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from './../users/users.module';
 import { AuthRepository } from './auth.repository';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: '7d',
+      },
+    }),
+  ],
   providers: [AuthService, AuthRepository],
   controllers: [AuthController],
 })
