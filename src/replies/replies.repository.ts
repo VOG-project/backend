015ef4605f9cbd3ce9c57d1bf7fea896c315270f
HyunSeong <hhs950120@gmail.com>
2023-04-14 05:56:44 +0900

    FEAT: 답글 생성 메소드 생성

--- src/replies/replies.repository.ts
+++ src/replies/replies.repository.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ReplyEntity } from './replies.entity';
 import { Repository } from 'typeorm';
+import { ReplyRegisterRequest } from './dto/register.reply.dto';
 
 @Injectable()
 export class RepliesRepository {
@@ -9,4 +10,18 @@ export class RepliesRepository {
     @InjectRepository(ReplyEntity)
     private readonly replyModel: Repository<ReplyEntity>,
   ) {}
+
+  async create(replyRegisterRequest: ReplyRegisterRequest) {
+    try {
+      const insertedReply = await this.replyModel
+        .createQueryBuilder()
+        .insert()
+        .values(replyRegisterRequest)
+        .execute();
+
+      return { replyId: insertedReply.identifiers[0].id };
+    } catch (err) {
+      throw new HttpException(`[MYSQL ERROR] create: ${err.message}`, 500);
+    }
+  }
 }
