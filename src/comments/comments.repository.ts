2a70e6b48700e2f41240ae233c082a2798970109
HyunSeong <hhs950120@gmail.com>
2023-04-14 04:35:46 +0900

    FEAT: 댓글 생성 메소드 생성

--- src/comments/comments.repository.ts
+++ src/comments/comments.repository.ts
@@ -1,10 +1,24 @@
 import { InjectRepository } from '@nestjs/typeorm';
 import { CommentEntity } from './comments.entity';
 import { Repository } from 'typeorm';
+import { CommentRegisterRequest } from './dto/register.comment.dto';
+import { HttpException } from '@nestjs/common';
 
 export class CommentsRepository {
   constructor(
     @InjectRepository(CommentEntity)
     private readonly commentModel: Repository<CommentEntity>,
   ) {}
+
+  async create(commentRegisterRequest: CommentRegisterRequest): Promise<void> {
+    try {
+      await this.commentModel
+        .createQueryBuilder()
+        .insert()
+        .values(commentRegisterRequest)
+        .execute();
+    } catch (err) {
+      throw new HttpException(`[MYSQL ERROR] create: ${err.message}`, 500);
+    }
+  }
 }
