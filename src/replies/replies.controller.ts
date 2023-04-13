ea0b09ea034c473b146449c5f97f654214bc517d
HyunSeong <hhs950120@gmail.com>
2023-04-14 06:01:17 +0900

    FEAT: 답글 생성 메소드 생성

--- src/replies/replies.controller.ts
+++ src/replies/replies.controller.ts
@@ -1,7 +1,39 @@
 import { Controller } from '@nestjs/common';
 import { RepliesService } from './replies.service';
+import {
+  Controller,
+  UseFilters,
+  UseInterceptors,
+  Body,
+  Get,
+  Post,
+  Delete,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ReplyRegisterRequest } from './dto/register.reply.dto';
+import { ReplyEntireDataReturn } from './dto/return.reply.dto';
 
 @Controller('replies')
 export class RepliesController {
   constructor(private readonly replyService: RepliesService) {}
+
+  @Post()
+  @ApiOperation({
+    summary: '답글 생성 API',
+    tags: ['Replies'],
+  })
+  @ApiResponse({
+    description: '생성된 답글의 전체 데이터를 반환합니다.',
+    type: ReplyEntireDataReturn,
+  })
+  async registerReply(
+    @Body() replyRegisterRequest: ReplyRegisterRequest,
+  ): Promise<ReplyEntireDataReturn> {
+    return await this.replyService.registerReply(replyRegisterRequest);
+  }
 }
--- src/replies/replies.service.ts
+++ src/replies/replies.service.ts
@@ -1,7 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { RepliesRepository } from './replies.repository';
+import { ReplyRegisterRequest } from './dto/register.reply.dto';
+import { ReplyEntireDataReturn } from './dto/return.reply.dto';
 
 @Injectable()
 export class RepliesService {
   constructor(private readonly replyRepository: RepliesRepository) {}
+
+  async registerReply(
+    replyRegisterRequest: ReplyRegisterRequest,
+  ): Promise<ReplyEntireDataReturn> {
+    const { replyId } = await this.replyRepository.create(replyRegisterRequest);
+    return await this.replyRepository.findByReplyId(replyId);
+  }
 }
