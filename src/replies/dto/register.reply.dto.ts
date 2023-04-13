61b412201c15357161feac9d260531fe90d3a4b5
HyunSeong <hhs950120@gmail.com>
2023-04-14 05:53:12 +0900

    FEAT: 댓글 생성 요청 DTO 생성

---
+++ src/replies/dto/register.reply.dto.ts
@@ -0,0 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsInt, IsNotEmpty, IsString } from 'class-validator';
+
+export class ReplyRegisterRequest {
+  @ApiProperty({
+    example: 36,
+    description: '답글 작성자 Id',
+  })
+  @IsNotEmpty()
+  @IsInt()
+  writerId: number;
+
+  @ApiProperty({
+    example: 312,
+    description: '답글이 작성된 댓글 Id',
+  })
+  @IsNotEmpty()
+  @IsInt()
+  commentId: number;
+
+  @ApiProperty({
+    example: '답글 달아봅니다~',
+    description: '답글 내용',
+  })
+  @IsNotEmpty()
+  @IsString()
+  content: string;
+}
