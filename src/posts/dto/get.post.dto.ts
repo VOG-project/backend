42dffb46d4199a78fd47e10a5781dd877a065a94
HyunSeong <hhs950120@gmail.com>
2023-04-14 22:33:07 +0900

    FEAT: 게시물 검색 쿼리 DTO 생성

--- src/posts/dto/get.post.dto.ts
+++ src/posts/dto/get.post.dto.ts
@@ -1,6 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsInt, IsNotEmpty, IsString } from 'class-validator';
 
+enum SearchTypeEnum {
+  닉네임,
+  제목,
+}
+
 export class PostGetCondition {
   @ApiProperty({
     example: 'championship',
@@ -17,4 +22,40 @@ export class PostGetCondition {
   @IsNotEmpty()
   @IsInt()
   page: number;
+}
+
+export class PostSearchRequest {
+  @ApiProperty({
+    example: '꾸꾸까까',
+    description: '검색어',
+  })
+  @IsNotEmpty()
+  @IsString()
+  keyword: string;
+}
+
+export class PostSearchCondition {
+  @ApiProperty({
+    example: '닉네임',
+    description: '어떤 키워드로 검색할 것인지',
+  })
+  @IsNotEmpty()
+  @IsString()
+  searchType: string;
+
+  @ApiProperty({
+    example: 3,
+    description: '불러올 페이지 번호',
+  })
+  @IsNotEmpty()
+  @IsInt()
+  page: number;
+
+  @ApiProperty({
+    example: 'championship',
+    description: '게시판 카테고리 명(free | humor | championship)',
+  })
+  @IsNotEmpty()
+  @IsString()
+  board: string;
 }
