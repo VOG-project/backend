bf9e530aa2447a9c9b7317390f8b6c9363e796e9
HyunSeong <hhs950120@gmail.com>
2023-04-14 14:58:50 +0900

    FEAT: 답글 API 생성 - 컨트롤러, 서비스

--- src/replies/replies.controller.ts
+++ src/replies/replies.controller.ts
@@ -1,4 +1,3 @@
-import { Controller } from '@nestjs/common';
 import { RepliesService } from './replies.service';
 import {
   Controller,
@@ -17,6 +16,7 @@ import {
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { ReplyRegisterRequest } from './dto/register.reply.dto';
 import { ReplyEntireDataReturn } from './dto/return.reply.dto';
+import { ReplyModifyRequest } from './dto/modify.reply.dto';
 
 @Controller('replies')
 export class RepliesController {
@@ -35,5 +35,20 @@ export class RepliesController {
     @Body() replyRegisterRequest: ReplyRegisterRequest,
   ): Promise<ReplyEntireDataReturn> {
     return await this.replyService.registerReply(replyRegisterRequest);
+  }
+
+  @Patch(':replyId')
+  @ApiOperation({
+    summary: '댓글 수정 API',
+    tags: ['Replies'],
+  })
+  @ApiResponse({
+    description: '수정한 댓글에 대한 모든 데이터를 반환합니다.',
+  })
+  async modifyReply(
+    @Body() replyModifyRequest: ReplyModifyRequest,
+    @Param('replyId', ParseIntPipe) replyId: number,
+  ): Promise<ReplyEntireDataReturn> {
+    return await this.replyService.modifyReply(replyModifyRequest, replyId);
   }
 }
--- src/replies/replies.service.ts
+++ src/replies/replies.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException } from '@nestjs/common';
 import { RepliesRepository } from './replies.repository';
 import { ReplyRegisterRequest } from './dto/register.reply.dto';
 import { ReplyEntireDataReturn } from './dto/return.reply.dto';
+import { ReplyModifyRequest } from './dto/modify.reply.dto';
 
 @Injectable()
 export class RepliesService {
@@ -11,6 +12,18 @@ export class RepliesService {
     replyRegisterRequest: ReplyRegisterRequest,
   ): Promise<ReplyEntireDataReturn> {
     const { replyId } = await this.replyRepository.create(replyRegisterRequest);
+    return await this.replyRepository.findByReplyId(replyId);
+  }
+
+  async modifyReply(
+    replyModifyRequest: ReplyModifyRequest,
+    replyId: number,
+  ): Promise<ReplyEntireDataReturn> {
+    const isExistedReply = await this.replyRepository.checkExist(replyId);
+    if (!isExistedReply)
+      throw new HttpException('존재하지 않는 답글입니다.', 400);
+
+    await this.replyRepository.update(replyModifyRequest, replyId);
     return await this.replyRepository.findByReplyId(replyId);
   }
 }
