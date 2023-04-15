28c9254d9eb33ae268b6b153e3c91edc6b187a45
HyunSeong <hhs950120@gmail.com>
2023-04-15 20:33:15 +0900

    FEAT: AUthRepository export

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,14 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { Module, forwardRef, Global } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from './../users/users.module';
 import { AuthRepository } from './auth.repository';
 
+@Global()
 @Module({
   imports: [forwardRef(() => UsersModule)],
   providers: [AuthService, AuthRepository],
   controllers: [AuthController],
-  exports: [AuthService],
+  exports: [AuthService, AuthRepository],
 })
 export class AuthModule {}
