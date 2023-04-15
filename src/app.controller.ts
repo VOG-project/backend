477ee2c273d09b558cc19433e1d5d7b6a64fe020
HyunSeong <hhs950120@gmail.com>
2023-04-15 22:20:30 +0900

    FIX: 사용하지 않는 API 삭제

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,24 +1,11 @@
-import {
-  Controller,
-  UseGuards,
-  UseFilters,
-  UseInterceptors,
-  Get,
-} from '@nestjs/common';
+import { Controller, UseFilters, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { SuccessInterceptor } from './common/interceptors/success.interceptor';
-import { AuthGuard } from './auth/guards/auth.guard';
 
 @Controller()
 @UseFilters(HttpExceptionFilter)
 @UseInterceptors(SuccessInterceptor)
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get('test')
-  @UseGuards(AuthGuard)
-  abc() {
-    return 'abcdefg';
-  }
 }
