ea627f209a198467e7116142d6c1c429ef159556
HyunSeong <bolton7425@gmail.com>
2023-05-08 16:12:43 +0900

    FEAT: 유저 서비스의 removeUser 메소드에 대한 단위 테스트 작성

--- src/users/tests/mocks/users.repository.mock.ts
+++ src/users/tests/mocks/users.repository.mock.ts
@@ -1,15 +1,23 @@
-import { setUserReturnDummy } from '../dummies/users.return.dummy';
+import {
+  setDeletedRowReturnDummy,
+  setUserReturnDummy,
+} from '../dummies/users.return.dummy';
 
 export const mockUserRepository = () => {
   const userReturn = setUserReturnDummy();
+  const deletedUserReturn = setDeletedRowReturnDummy();
 
   return {
     updateProfileUrl: jest.fn(),
     update: jest.fn(),
-    findOneById: jest.fn().mockResolvedValue(userReturn),
+    // userId가 35가 아니면 에러 발생하도록
+    findOneById: jest.fn((userId) => {
+      if (userId === 35) return userReturn;
+      else return null;
+    }),
     findOneByOAuthId: jest.fn(),
     create: jest.fn(),
     findByNickname: jest.fn(),
-    deleteById: jest.fn(),
+    deleteById: jest.fn().mockResolvedValue(deletedUserReturn),
   };
 };
--- src/users/tests/users.service.spec.ts
+++ src/users/tests/users.service.spec.ts
@@ -4,12 +4,17 @@ import { UserService } from '../users.service';
 import { AuthService } from 'src/auth/auth.service';
 import { mockAuthService } from 'src/auth/tests/mocks/auth.service.mock';
 import { mockUserRepository } from './mocks/users.repository.mock';
-import { setUserReturnDummy } from './dummies/users.return.dummy';
+import {
+  setDeletedRowReturnDummy,
+  setUserReturnDummy,
+} from './dummies/users.return.dummy';
+import { HttpException } from '@nestjs/common';
 
 describe('UserService', () => {
   let userService: UserService;
   let userRepository: UserRepository;
   const userReturn = setUserReturnDummy();
+  const deletedUserReturn = setDeletedRowReturnDummy();
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -34,6 +39,20 @@ describe('UserService', () => {
     it('SUCCESS: 유저 ID에 해당하는 유저 데이터를 반환', async () => {
       expect(await userService.getUser(35)).toEqual(userReturn);
       expect(userRepository.findOneById).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('Remove User Data', () => {
+    it('SUCCESS: 유저 ID에 해당하는 유저 데이터를 삭제하고 유저 엔티티에서 삭제된 row 개수 반환', async () => {
+      expect(await userService.removeUser(35)).toEqual(deletedUserReturn);
+      expect(userRepository.findOneById).toHaveBeenCalledTimes(1);
+      expect(userRepository.deleteById).toHaveBeenCalledTimes(1);
+    });
+
+    it('EXCEPTION: 유저 ID에 해당하는 데이터가 존재하지 않아 예외 발생', async () => {
+      expect(async () => await userService.removeUser(40)).rejects.toThrow(
+        '존재하지 않는 유저입니다.',
+      );
     });
   });
 });
