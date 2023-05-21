cbc1177c6ac07b887cb583cc9fdd23d5cb14db3f
HyunSeong <bolton7425@gmail.com>
2023-05-21 21:48:15 +0900

    FEAT: 정상적인 답글 생성 단위 테스트 작성

--- src/replies/tests/replies.controller.spec.ts
+++ src/replies/tests/replies.controller.spec.ts
@@ -3,20 +3,40 @@ import { RepliesController } from '../replies.controller';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
 import { mockAuthGuard } from 'src/auth/tests/mocks/auth.guard.mock';
 import { ReplyRegisterDto } from './dummies/replies.dto.dummy';
+import { RepliesService } from '../replies.service';
+import { ReplyReturn } from './dummies/replies.return.dummy';
 
 describe('RepliesController', () => {
   let repliesController: RepliesController;
+  let repliesService: RepliesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [RepliesController],
+      providers: [
+        {
+          provide: RepliesService,
+          useValue: {
+            registerReply: jest.fn(() => ReplyReturn),
+          },
+        },
+      ],
     })
       .overrideGuard(AuthGuard)
       .useValue(mockAuthGuard())
       .compile();
 
     repliesController = module.get<RepliesController>(RepliesController);
+    repliesService = module.get<RepliesService>(RepliesService);
   });
 
-  
+  describe('RegisterReply', () => {
+    it('SUCCESS: 정상적인 Dto 값을 받으면 데이터 반환 ', async () => {
+      const replyDummyDto = ReplyRegisterDto;
+      const res = await repliesController.registerReply(replyDummyDto);
+      expect(res).toStrictEqual(ReplyReturn);
+      expect(repliesService.registerReply).toBeCalledTimes(1);
+      expect(repliesService.registerReply).toBeCalledWith(replyDummyDto);
+    });
+  });
 });
