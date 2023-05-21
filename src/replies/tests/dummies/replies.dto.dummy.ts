5072dccd2ffd45fb440f219b61f6931905e1873b
HyunSeong <bolton7425@gmail.com>
2023-05-21 21:19:50 +0900

    FEAT: 답글 등록 더미 DTO 데이터 생성

---
+++ src/replies/tests/dummies/replies.dto.dummy.ts
@@ -0,0 +1,8 @@
+import { ReplyRegisterRequest } from 'src/replies/dto/register.reply.dto';
+
+export const ReplyRegisterDto: ReplyRegisterRequest = {
+  writerId: 1,
+  commentId: 10,
+  postId: 100,
+  content: '답글 테스트',
+};
