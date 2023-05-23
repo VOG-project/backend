666db2bde2e61ed8438312ae3f4604b7deac82e7
HyunSeong <bolton7425@gmail.com>
2023-05-24 00:30:48 +0900

    FEAT: 친구 삭제 테스트용 더미 Return 생성

--- src/friend/tests/dummies/friend.return.dummy.ts
+++ src/friend/tests/dummies/friend.return.dummy.ts
@@ -1,4 +1,4 @@
-import { FriendFollowingReturn } from 'src/friend/dto/return.friend.dto';
+import { FriendDeletedCountReturn } from 'src/friend/dto/return.friend.dto';
 import { UserEntireDataReturn } from 'src/users/dto/return.user.dto';
 
 interface IFriendFollowing {
@@ -19,4 +19,19 @@ export const FriendReturn: IFriendFollowing = {
     profileUrl:
       'https://vog-image-storage.s3.ap-northeast-2.amazonaws.com/user/4-p.jpg',
   },
+};
+
+export const UserReturn: UserEntireDataReturn = {
+  id: 1,
+  oauthId: 'Sb35YY9N_bZgbfSW1jDYkjCcgKrEEHUQ8CLTn',
+  provider: 'naver',
+  nickname: '테스트',
+  sex: '남',
+  profileUrl: 'https://www.vog-storage/user/efsef.jpg',
+  createdAt: new Date('2023-03-05 16:25:04.871850'),
+  updatedAt: new Date('2023-03-05 16:25:04.871850'),
+};
+
+export const FriendDeletedRowCountReturn: FriendDeletedCountReturn = {
+  deletedCount: 1,
 };
