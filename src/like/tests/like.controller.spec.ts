587a9c8e2b2caf6f052945d67b7057c7f4a14692
HyunSeong <bolton7425@gmail.com>
2023-05-25 05:00:51 +0900

    FEAT: 좋아요 등록 단위 테스트 작성
    - 성공 케이스

--- src/like/tests/like.controller.spec.ts
+++ src/like/tests/like.controller.spec.ts
@@ -3,6 +3,8 @@ import { LikeController } from '../like.controller';
 import { LikeService } from '../like.service';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
 import { mockAuthGuard } from 'src/auth/tests/mocks/auth.guard.mock';
+import { LikeRegisterDto } from './dummies/like.dto.dummy';
+import { LikeUsers } from './dummies/like.return.dummy';
 
 describe('LikeController', () => {
   let likeController: LikeController;
@@ -14,7 +16,9 @@ describe('LikeController', () => {
       providers: [
         {
           provide: LikeService,
-          useValue: {},
+          useValue: {
+            registerLike: jest.fn(() => LikeUsers),
+          },
         },
       ],
     })
@@ -26,5 +30,17 @@ describe('LikeController', () => {
     likeService = module.get<LikeService>(LikeService);
   });
 
-  describe('')
+  describe('RegisterLike', () => {
+    it('SUCCESS: Dto에 포함된 userId와 postId를 입력 받아 좋아요 추가 및 postId에 해당하는 게시물에 좋아요를 누른 userId 반환', async () => {
+      const likeDummyDto = LikeRegisterDto;
+      const postId = 1;
+      const likeUsersReturn = LikeUsers;
+
+      const result = await likeController.registerLike(likeDummyDto, postId);
+
+      expect(result).toStrictEqual(likeUsersReturn);
+      expect(likeService.registerLike).toBeCalledTimes(1);
+      expect(likeService.registerLike).toBeCalledWith(postId, likeDummyDto);
+    });
+  });
 });
