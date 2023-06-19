1df37b9fee11cbab72e7886a55a7b85763267d68
HyunSeong <bolton7425@gmail.com>
2023-06-20 07:18:24 +0900

    FEAT: 좋아요 단위 테스트 작성(성공케이스)
    - cancelLike 메소드 테스트

--- src/like/tests/like.service.spec.ts
+++ src/like/tests/like.service.spec.ts
@@ -1,7 +1,7 @@
 import { LikeRepository } from '../like.repository';
 import { LikeService } from './../like.service';
 import { Test, TestingModule } from '@nestjs/testing';
-import { LikeRegisterDto } from './dummies/like.dto.dummy';
+import { LikeDeleteDto, LikeRegisterDto } from './dummies/like.dto.dummy';
 import { LikeUsers, PostReturn, UserReturn } from './dummies/like.return.dummy';
 import { UserRepository } from './../../users/users.repository';
 import { PostsRepository } from './../../posts/posts.repository';
@@ -22,6 +22,7 @@ describe('LikeService', () => {
           useValue: {
             findLikeUsersByPostId: jest.fn(() => LikeUsers),
             createLike: jest.fn(),
+            deleteLike: jest.fn(),
           },
         },
         {
@@ -89,5 +90,30 @@ describe('LikeService', () => {
       expect(userRepository.findOneById).toBeCalledTimes(1);
       expect(userRepository.findOneById).toBeCalledWith(likeDummyDto.userId);
     });
+  });
+
+  describe('CancelLike', () => {
+    const likeDummyDto = LikeDeleteDto;
+    const postId = 1;
+    const likeUsersReturn = LikeUsers;
+
+    it('SUCCESS: 좋아요 대상 userId와 postId 입력받아 좋아요 취소 및 postId에 해당하는 게시물에 좋아요를 누른 userId 반환', async () => {
+      const result = await likeService.cancelLike(postId, likeDummyDto);
+
+      expect(result).toStrictEqual(likeUsersReturn);
+      expect(userRepository.findOneById).toBeCalledTimes(1);
+      expect(userRepository.findOneById).toBeCalledWith(likeDummyDto.userId);
+      expect(postRepository.findOneById).toBeCalledTimes(1);
+      expect(postRepository.findOneById).toBeCalledWith(postId);
+      expect(likeRepository.deleteLike).toBeCalledTimes(1);
+      expect(likeRepository.deleteLike).toBeCalledWith(
+        postId,
+        likeDummyDto.userId,
+      );
+      expect(likeRepository.findLikeUsersByPostId).toBeCalledTimes(1);
+      expect(likeRepository.findLikeUsersByPostId).toBeCalledWith(postId);
+    });
+
+    
   });
 });
