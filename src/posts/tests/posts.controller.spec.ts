ab4f502258baf51ea8a23a2d0b205687f1c2b7f2
HyunSeong <bolton7425@gmail.com>
2023-07-01 05:30:18 +0900

    FEAT: Post 단위 테스트 작성 (성공 케이스)
    - registerPosts 메소드 테스트

--- src/posts/tests/posts.controller.spec.ts
+++ src/posts/tests/posts.controller.spec.ts
@@ -1,18 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostsController } from '../posts.controller';
+import { PostsService } from '../posts.service';
+import { AuthGuard } from 'src/auth/guards/auth.guard';
+import { mockAuthGuard } from 'src/auth/tests/mocks/auth.guard.mock';
+import { PostRegisterDummyDto } from './dummies/posts.dto.dummy';
+import { PostEntireReturn } from './dummies/posts.return.dummy';
 
 describe('PostsController', () => {
-  let controller: PostsController;
+  let postsController: PostsController;
+  let postsService: PostsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PostsController],
-    }).compile();
+      providers: [
+        {
+          provide: PostsService,
+          useValue: { registerPost: jest.fn(() => PostEntireReturn) },
+        },
+      ],
+    })
+      .overrideGuard(AuthGuard)
+      .useValue(mockAuthGuard())
+      .compile();
 
-    controller = module.get<PostsController>(PostsController);
+    postsController = module.get<PostsController>(PostsController);
+    postsService = module.get<PostsService>(PostsService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('RegisterPost', () => {
+    const postDummyDto = PostRegisterDummyDto;
+    const postReturn = PostEntireReturn;
+
+    it('SUCCESS: 댓글 데이터 생성 및 생성된 데이터 반환', async () => {
+      const result = await postsController.registerPost(postDummyDto);
+
+      expect(result).toStrictEqual(postReturn);
+      expect(postsService.registerPost).toBeCalledTimes(1);
+      expect(postsService.registerPost).toBeCalledWith(postDummyDto);
+    });
   });
 });
