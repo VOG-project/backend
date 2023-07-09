8390b729d1d2c7385875c4dcc8deeb1cca0277b1
HyunSeong <bolton7425@gmail.com>
2023-07-09 22:48:20 +0900

    FEAT: posts 서비스 registerPost 메소드 단위 테스트
    - 성공 케이스

--- src/posts/tests/posts.service.spec.ts
+++ src/posts/tests/posts.service.spec.ts
@@ -18,7 +18,15 @@ describe('PostsService', () => {
         PostsService,
         {
           provide: PostsRepository,
-          useValue: {},
+          useValue: {
+            create: jest.fn(() => {
+              return {
+                postId: 1,
+              };
+            }),
+            createView: jest.fn(),
+            findOneById: jest.fn(() => PostEntireReturn),
+          },
         },
         {
           provide: LikeRepository,
@@ -35,5 +43,23 @@ describe('PostsService', () => {
     postsRepository = module.get<PostsRepository>(PostsRepository);
     likeRepository = module.get<LikeRepository>(LikeRepository);
     commentRepository = module.get<CommentsRepository>(CommentsRepository);
+  });
+
+  describe('RegisterPost', () => {
+    const postId = 1;
+    const postDummyDto = PostRegisterDummyDto;
+    const postReturn = PostEntireReturn;
+
+    it('SUCCESS: 댓글 데이터 생성 및 해당 데이터 반환', async () => {
+      const result = await postsService.registerPost(postDummyDto);
+
+      expect(result).toStrictEqual(postReturn);
+      expect(postsRepository.create).toBeCalledTimes(1);
+      expect(postsRepository.create).toBeCalledWith(postDummyDto);
+      expect(postsRepository.createView).toBeCalledTimes(1);
+      expect(postsRepository.createView).toBeCalledWith(postId);
+      expect(postsRepository.findOneById).toBeCalledTimes(1);
+      expect(postsRepository.findOneById).toBeCalledWith(postId);
+    });
   });
 });
