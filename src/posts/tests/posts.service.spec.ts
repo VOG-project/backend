8484efef00aba77a89701f1d8af7d76134231bd2
HyunSeong <bolton7425@gmail.com>
2023-07-09 22:40:55 +0900

    FEAT: Repository 의존성 추가

--- src/posts/tests/posts.service.spec.ts
+++ src/posts/tests/posts.service.spec.ts
@@ -1,10 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostsRepository } from '../posts.repository';
 import { PostsService } from '../posts.service';
+import { PostRegisterDummyDto } from './dummies/posts.dto.dummy';
+import { PostEntireReturn } from './dummies/posts.return.dummy';
+import { LikeRepository } from 'src/like/like.repository';
+import { CommentsRepository } from 'src/comments/comments.repository';
 
 describe('PostsService', () => {
   let postsService: PostsService;
   let postsRepository: PostsRepository;
+  let likeRepository: LikeRepository;
+  let commentRepository: CommentsRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -14,11 +20,20 @@ describe('PostsService', () => {
           provide: PostsRepository,
           useValue: {},
         },
+        {
+          provide: LikeRepository,
+          useValue: {},
+        },
+        {
+          provide: CommentsRepository,
+          useValue: {},
+        },
       ],
     }).compile();
 
     postsService = module.get<PostsService>(PostsService);
     postsRepository = module.get<PostsRepository>(PostsRepository);
+    likeRepository = module.get<LikeRepository>(LikeRepository);
+    commentRepository = module.get<CommentsRepository>(CommentsRepository);
   });
-
 });
